c6e9b318dec8b47e3c841f33677276d5efd9750e
John Johnston <johnjohnston@mac.com>
2018-05-23 01:17:49 +0100

    Added 2 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16667,
-	"created_at": "Tue May 22 00:17:48 +0000 2018",
+	"tweets": 16669,
+	"created_at": "Wed May 23 00:17:48 +0000 2018",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2018_05.js
+++ tweets/data/js/tweets/2018_05.js
@@ -1,6 +1,625 @@
 Grailbird.data.tweets_2018_05 = 
 [
 	{
+		"created_at": "Tue May 22 21:45:31 +0000 2018",
+		"id": 999043603635859500,
+		"id_str": "999043603635859456",
+		"text": "RT @BBCTrustMe: @BBCTwo and @DrMichaelMosley are coming to #Glasgow - Do any Glaswegians fancy learning #Spanish with us? https://t.co/6u4m…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Glasgow",
+					"indices": [
+						59,
+						67
+					]
+				},
+				{
+					"text": "Spanish",
+					"indices": [
+						104,
+						112
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BBCTrustMe",
+					"name": "Trust Me I'm aDoctor",
+					"id": 712664728598917100,
+					"id_str": "712664728598917120",
+					"indices": [
+						3,
+						14
+					]
+				},
+				{
+					"screen_name": "BBCTwo",
+					"name": "BBC Two",
+					"id": 1586183960,
+					"id_str": "1586183960",
+					"indices": [
+						16,
+						23
+					]
+				},
+				{
+					"screen_name": "DrMichaelMosley",
+					"name": "Michael Mosley",
+					"id": 130197701,
+					"id_str": "130197701",
+					"indices": [
+						28,
+						44
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3899,
+			"friends_count": 2532,
+			"listed_count": 281,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4157,
+			"utc_offset": 3600,
+			"time_zone": "Edinburgh",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16609,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon May 21 09:29:29 +0000 2018",
+			"id": 998495990486327300,
+			"id_str": "998495990486327296",
+			"text": "@BBCTwo and @DrMichaelMosley are coming to #Glasgow - Do any Glaswegians fancy learning #Spanish with us? https://t.co/6u4mFwzgwF",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Glasgow",
+						"indices": [
+							43,
+							51
+						]
+					},
+					{
+						"text": "Spanish",
+						"indices": [
+							88,
+							96
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "BBCTwo",
+						"name": "BBC Two",
+						"id": 1586183960,
+						"id_str": "1586183960",
+						"indices": [
+							0,
+							7
+						]
+					},
+					{
+						"screen_name": "DrMichaelMosley",
+						"name": "Michael Mosley",
+						"id": 130197701,
+						"id_str": "130197701",
+						"indices": [
+							12,
+							28
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 998495988959588400,
+						"id_str": "998495988959588352",
+						"indices": [
+							106,
+							129
+						],
+						"media_url": "http://pbs.twimg.com/media/Ddtez8uWsAA_wla.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ddtez8uWsAA_wla.jpg",
+						"url": "https://t.co/6u4mFwzgwF",
+						"display_url": "pic.twitter.com/6u4mFwzgwF",
+						"expanded_url": "https://twitter.com/BBCTrustMe/status/998495990486327296/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 551,
+								"h": 300,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 551,
+								"h": 300,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 551,
+								"h": 300,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 998495988959588400,
+						"id_str": "998495988959588352",
+						"indices": [
+							106,
+							129
+						],
+						"media_url": "http://pbs.twimg.com/media/Ddtez8uWsAA_wla.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ddtez8uWsAA_wla.jpg",
+						"url": "https://t.co/6u4mFwzgwF",
+						"display_url": "pic.twitter.com/6u4mFwzgwF",
+						"expanded_url": "https://twitter.com/BBCTrustMe/status/998495990486327296/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 551,
+								"h": 300,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 551,
+								"h": 300,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 551,
+								"h": 300,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": 1586183960,
+			"in_reply_to_user_id_str": "1586183960",
+			"in_reply_to_screen_name": "BBCTwo",
+			"user": {
+				"id": 712664728598917100,
+				"id_str": "712664728598917120",
+				"name": "Trust Me I'm aDoctor",
+				"screen_name": "BBCTrustMe",
+				"location": "United Kingdom",
+				"description": "The official account for BBC Two's flagship health and medicine series Trust Me, I'm a Doctor.",
+				"url": "https://t.co/PjdeD6G722",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/PjdeD6G722",
+								"expanded_url": "http://www.bbc.co.uk/programmes/b04j9gny",
+								"display_url": "bbc.co.uk/programmes/b04…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6931,
+				"friends_count": 382,
+				"listed_count": 53,
+				"created_at": "Wed Mar 23 15:38:06 +0000 2016",
+				"favourites_count": 943,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 957,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/912599853888393216/jd0IMNU__normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/912599853888393216/jd0IMNU__normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/712664728598917120/1507037935",
+				"profile_link_color": "19CF86",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6,
+			"favorite_count": 4,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
+		"created_at": "Tue May 22 15:33:30 +0000 2018",
+		"id": 998949982362390500,
+		"id_str": "998949982362390529",
+		"text": "I’ll be here: https://t.co/Nia9IK2bF8",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Nia9IK2bF8",
+					"expanded_url": "https://twitter.com/mtimmonsmusic/status/998464694234075136",
+					"display_url": "twitter.com/mtimmonsmusic/…",
+					"indices": [
+						14,
+						37
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3899,
+			"friends_count": 2532,
+			"listed_count": 281,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4157,
+			"utc_offset": 3600,
+			"time_zone": "Edinburgh",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16609,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "539f5ae54d241371",
+			"url": "https://api.twitter.com/1.1/geo/id/539f5ae54d241371.json",
+			"place_type": "city",
+			"name": "Banton",
+			"full_name": "Banton, Scotland",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-4.009223,
+							55.986072
+						],
+						[
+							-3.998547,
+							55.986072
+						],
+						[
+							-3.998547,
+							55.991639
+						],
+						[
+							-4.009223,
+							55.991639
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 998464694234075100,
+		"quoted_status_id_str": "998464694234075136",
+		"quoted_status": {
+			"created_at": "Mon May 21 07:25:08 +0000 2018",
+			"id": 998464694234075100,
+			"id_str": "998464694234075136",
+			"text": "I play Kilmarnock Train Station, Killie Browser this Friday 25th, 7pm. Tickets available on the door. @KSCVillage… https://t.co/hJQjGdtrzn",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "KSCVillage",
+						"name": "KSRHT",
+						"id": 787968517211582500,
+						"id_str": "787968517211582464",
+						"indices": [
+							102,
+							113
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/hJQjGdtrzn",
+						"expanded_url": "https://twitter.com/i/web/status/998464694234075136",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							115,
+							138
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1244438078,
+				"id_str": "1244438078",
+				"name": "Michael Timmons",
+				"screen_name": "MTImmonsMusic",
+				"location": "",
+				"description": "Musician - Glasgow. Debut album Bone Coloured out now. https://t.co/7o25Cj4Iud",
+				"url": "https://t.co/zTmItDfH0P",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/zTmItDfH0P",
+								"expanded_url": "https://open.spotify.com/album/0Fm4ck7eQ7xUtaTWuPeM3B?si=wn1N_Zp6SCmfkMGcq9Ei1Q",
+								"display_url": "open.spotify.com/album/0Fm4ck7e…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/7o25Cj4Iud",
+								"expanded_url": "http://michaeltimmonsmusic.bandcamp.com",
+								"display_url": "michaeltimmonsmusic.bandcamp.com",
+								"indices": [
+									55,
+									78
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 458,
+				"friends_count": 697,
+				"listed_count": 12,
+				"created_at": "Tue Mar 05 19:16:35 +0000 2013",
+				"favourites_count": 2164,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 1702,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/848934481096474624/qKZCEFdR_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/848934481096474624/qKZCEFdR_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1244438078/1520462709",
+				"profile_link_color": "141414",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 12,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon May 21 18:52:16 +0000 2018",
 		"id": 998637616122748900,
 		"id_str": "998637616122748928",
