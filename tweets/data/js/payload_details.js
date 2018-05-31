31d602578d0b488aeff01715f528b34ddaa7ceae
John Johnston <johnjohnston@mac.com>
2018-05-31 01:17:49 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16683,
-	"created_at": "Wed May 30 00:17:48 +0000 2018",
+	"tweets": 16684,
+	"created_at": "Thu May 31 00:17:48 +0000 2018",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2018_05.js
+++ tweets/data/js/tweets/2018_05.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2018_05 = 
 [
 	{
+		"created_at": "Wed May 30 18:14:25 +0000 2018",
+		"id": 1001889583020281900,
+		"id_str": "1001889583020281861",
+		"text": "@WhiteleesPS @Schools_Digital @DigitalNL @pedagoo @LandLNLC @WhiteleesPF @DigiLearnScot @EducationScot @DL_for_All… https://t.co/PnDRTdS6JE",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "WhiteleesPS",
+					"name": "WhiteleesPS",
+					"id": 4081659989,
+					"id_str": "4081659989",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "Schools_Digital",
+					"name": "Digital Schools",
+					"id": 4151839535,
+					"id_str": "4151839535",
+					"indices": [
+						13,
+						29
+					]
+				},
+				{
+					"screen_name": "DigitalNL",
+					"name": "Digital North Lanarkshire",
+					"id": 938331784986677200,
+					"id_str": "938331784986677249",
+					"indices": [
+						30,
+						40
+					]
+				},
+				{
+					"screen_name": "pedagoo",
+					"name": "Pedagoo.org",
+					"id": 291838882,
+					"id_str": "291838882",
+					"indices": [
+						41,
+						49
+					]
+				},
+				{
+					"screen_name": "LandLNLC",
+					"name": "Education North Lan",
+					"id": 4004353900,
+					"id_str": "4004353900",
+					"indices": [
+						50,
+						59
+					]
+				},
+				{
+					"screen_name": "WhiteleesPF",
+					"name": "Whitelees Parent Council",
+					"id": 930064394284929000,
+					"id_str": "930064394284929024",
+					"indices": [
+						60,
+						72
+					]
+				},
+				{
+					"screen_name": "DigiLearnScot",
+					"name": "DigiLearn Community",
+					"id": 2965753499,
+					"id_str": "2965753499",
+					"indices": [
+						73,
+						87
+					]
+				},
+				{
+					"screen_name": "EducationScot",
+					"name": "Education Scotland",
+					"id": 314003959,
+					"id_str": "314003959",
+					"indices": [
+						88,
+						102
+					]
+				},
+				{
+					"screen_name": "DL_for_All",
+					"name": "Digital Learning for ALL",
+					"id": 970802301857423400,
+					"id_str": "970802301857423360",
+					"indices": [
+						103,
+						114
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/PnDRTdS6JE",
+					"expanded_url": "https://twitter.com/i/web/status/1001889583020281861",
+					"display_url": "twitter.com/i/web/status/1…",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1001874873281581000,
+		"in_reply_to_status_id_str": "1001874873281581057",
+		"in_reply_to_user_id": 4081659989,
+		"in_reply_to_user_id_str": "4081659989",
+		"in_reply_to_screen_name": "WhiteleesPS",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3900,
+			"friends_count": 2532,
+			"listed_count": 281,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4167,
+			"utc_offset": 3600,
+			"time_zone": "Edinburgh",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16623,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "791e00bcadc4615f",
+			"url": "https://api.twitter.com/1.1/geo/id/791e00bcadc4615f.json",
+			"place_type": "city",
+			"name": "Glasgow",
+			"full_name": "Glasgow, Scotland",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-4.3932845,
+							55.796184
+						],
+						[
+							-4.0902182,
+							55.796184
+						],
+						[
+							-4.0902182,
+							55.9204214
+						],
+						[
+							-4.3932845,
+							55.9204214
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue May 29 19:44:24 +0000 2018",
 		"id": 1001549838528471000,
 		"id_str": "1001549838528471040",
