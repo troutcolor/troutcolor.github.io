9754a14c461b7e1a9ce8b38e308ed862b94512c6
John Johnston <johnjohnston@mac.com>
2019-03-02 00:17:49 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17430,
-	"created_at": "Fri Mar 01 00:17:48 +0000 2019",
+	"tweets": 17431,
+	"created_at": "Sat Mar 02 00:17:48 +0000 2019",
 	"lang": "en"
 }
---
+++ tweets/data/js/tweets/2019_03.js
@@ -0,0 +1,274 @@
+Grailbird.data.tweets_2019_03 = 
+[
+	{
+		"created_at": "Fri Mar 01 08:55:25 +0000 2019",
+		"id": 1101405567904612400,
+		"id_str": "1101405567904612352",
+		"full_text": "Words fail.... https://t.co/bAcEBAuF7e",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			14
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bAcEBAuF7e",
+					"expanded_url": "https://twitter.com/markrpriestley/status/1101196005230460929",
+					"display_url": "twitter.com/markrpriestley…",
+					"indices": [
+						15,
+						38
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3910,
+			"friends_count": 2549,
+			"listed_count": 279,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4568,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17301,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "539f5ae54d241371",
+			"url": "https://api.twitter.com/1.1/geo/id/539f5ae54d241371.json",
+			"place_type": "city",
+			"name": "Banton",
+			"full_name": "Banton, Scotland",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-4.009223,
+							55.986072
+						],
+						[
+							-3.998547,
+							55.986072
+						],
+						[
+							-3.998547,
+							55.991639
+						],
+						[
+							-4.009223,
+							55.991639
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1101196005230460900,
+		"quoted_status_id_str": "1101196005230460929",
+		"quoted_status_permalink": {
+			"url": "https://t.co/bAcEBAuF7e",
+			"expanded": "https://twitter.com/markrpriestley/status/1101196005230460929",
+			"display": "twitter.com/markrpriestley…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Feb 28 19:02:41 +0000 2019",
+			"id": 1101196005230460900,
+			"id_str": "1101196005230460929",
+			"full_text": "Speaks volumes about their priorities. \nMPs debate climate after school strike – but only a handful turn up\n\nhttps://t.co/xc1tYnIYOR",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				132
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/xc1tYnIYOR",
+						"expanded_url": "https://www.theguardian.com/politics/2019/feb/28/mps-debate-climate-after-school-strike-but-only-a-handful-turn-up?CMP=Share_AndroidApp_Tweet",
+						"display_url": "theguardian.com/politics/2019/…",
+						"indices": [
+							109,
+							132
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 531346364,
+				"id_str": "531346364",
+				"name": "Mark Priestley 🇪🇺",
+				"screen_name": "MarkRPriestley",
+				"location": "University of Stirling",
+				"description": "Education Professor, University of Stirling. Lead editor of the Curriculum Journal. Interested in teachers and the curriculum. Blog at https://t.co/eZfvv8q5mx",
+				"url": "https://t.co/pzmPLTNQFr",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/pzmPLTNQFr",
+								"expanded_url": "http://rms.stir.ac.uk/converis-stirling/person/11016",
+								"display_url": "rms.stir.ac.uk/converis-stirl…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/eZfvv8q5mx",
+								"expanded_url": "http://mrpriestley.wordpress.com",
+								"display_url": "mrpriestley.wordpress.com",
+								"indices": [
+									135,
+									158
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 6088,
+				"friends_count": 1030,
+				"listed_count": 103,
+				"created_at": "Tue Mar 20 15:20:03 +0000 2012",
+				"favourites_count": 58,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 24452,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "030103",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1001534164489986053/zpUh1PMm_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1001534164489986053/zpUh1PMm_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/531346364/1439237749",
+				"profile_link_color": "947974",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "FA8459",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 4,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Words fail.... https://t.co/bAcEBAuF7e"
+	}
+]
