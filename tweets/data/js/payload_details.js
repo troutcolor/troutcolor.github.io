f6dbb8fd02e96552f6aff8ebff846e4f4b46be2f
John Johnston <johnjohnston@mac.com>
2019-05-04 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17560,
-	"created_at": "Fri May 03 00:17:47 +0000 2019",
+	"tweets": 17561,
+	"created_at": "Sat May 04 00:17:48 +0000 2019",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2019_05.js
+++ tweets/data/js/tweets/2019_05.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 03 16:51:02 +0000 2019",
+		"id": 1124355698475839500,
+		"id_str": "1124355698475839488",
+		"full_text": "99321 gifs and counting... My Raspberry Pi is getting near to 100000 gifs created and automatically  posted to tumblr counted on the Gif Cam page. (https://t.co/9oTbFWiKRm) https://t.co/j55P0scJci",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9oTbFWiKRm",
+					"expanded_url": "http://johnjohnston.info/blog/99321-gifs-and-counting/",
+					"display_url": "johnjohnston.info/blog/99321-gif…",
+					"indices": [
+						148,
+						171
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1124355696559120400,
+					"id_str": "1124355696559120385",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D5qDjIaX4AEmvFm.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D5qDjIaX4AEmvFm.jpg",
+					"url": "https://t.co/j55P0scJci",
+					"display_url": "pic.twitter.com/j55P0scJci",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1124355698475839488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 600,
+							"h": 450,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 600,
+							"h": 450,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 450,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1124355696559120400,
+					"id_str": "1124355696559120385",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D5qDjIaX4AEmvFm.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D5qDjIaX4AEmvFm.jpg",
+					"url": "https://t.co/j55P0scJci",
+					"display_url": "pic.twitter.com/j55P0scJci",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1124355698475839488/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"small": {
+							"w": 600,
+							"h": 450,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 600,
+							"h": 450,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 450,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							3
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/D5qDjIaX4AEmvFm.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3910,
+			"friends_count": 2552,
+			"listed_count": 279,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4658,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17473,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "99321 gifs and counting... My Raspberry Pi is getting near to 100000 gifs created and automatically  posted to tumblr counted on the Gif Cam page. (https://t.co/9oTbFWiKRm) https://t.co/j55P0scJci"
+	},
+	{
 		"created_at": "Thu May 02 17:01:36 +0000 2019",
 		"id": 1123995969610289200,
 		"id_str": "1123995969610289154",
