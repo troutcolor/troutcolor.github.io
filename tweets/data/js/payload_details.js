382d6835d05c793e71c6e15c351bd7de79efb002
John Johnston <johnjohnston@mac.com>
2019-07-05 01:17:48 +0100

    Added 2 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17659,
-	"created_at": "Thu Jul 04 00:17:47 +0000 2019",
+	"tweets": 17661,
+	"created_at": "Fri Jul 05 00:17:47 +0000 2019",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2019_07.js
+++ tweets/data/js/tweets/2019_07.js
@@ -1,6 +1,382 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Thu Jul 04 21:40:04 +0000 2019",
+		"id": 1146896482215301100,
+		"id_str": "1146896482215301122",
+		"full_text": "Yarrow https://t.co/xFLluDfoqc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			6
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1146896479270907900,
+					"id_str": "1146896479270907904",
+					"indices": [
+						7,
+						30
+					],
+					"media_url": "http://pbs.twimg.com/media/D-qYRXkWsAAQjM2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-qYRXkWsAAQjM2.jpg",
+					"url": "https://t.co/xFLluDfoqc",
+					"display_url": "pic.twitter.com/xFLluDfoqc",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1146896482215301122/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1801,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1146896479270907900,
+					"id_str": "1146896479270907904",
+					"indices": [
+						7,
+						30
+					],
+					"media_url": "http://pbs.twimg.com/media/D-qYRXkWsAAQjM2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-qYRXkWsAAQjM2.jpg",
+					"url": "https://t.co/xFLluDfoqc",
+					"display_url": "pic.twitter.com/xFLluDfoqc",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1146896482215301122/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1801,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://micro.blog/\" rel=\"nofollow\">Micro.blog</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3913,
+			"friends_count": 2554,
+			"listed_count": 274,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4726,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17608,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Yarrow https://t.co/xFLluDfoqc"
+	},
+	{
+		"created_at": "Thu Jul 04 09:50:55 +0000 2019",
+		"id": 1145779407744831500,
+		"id_str": "1146718021165207552",
+		"full_text": "🐉#DragonflyChallenge2019 🐉LEARN: FAQ we get ”How do you tell a Dragonfly from a Damselfly?\" If they fold wings down thier sides when perched you are watching a Damselfly. If they hold them out straight like the wings of an areoplane it’s a Dragonfly 🐉 https://t.co/tCQWl8kCrk https://t.co/8AS4wXwwUD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DragonflyChallenge2019",
+					"indices": [
+						1,
+						24
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tCQWl8kCrk",
+					"expanded_url": "http://bit.ly/2ROoQne",
+					"display_url": "bit.ly/2ROoQne",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1145779386618130400,
+					"id_str": "1145779386618130434",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D-agSBJWsAI3WPq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-agSBJWsAI3WPq.jpg",
+					"url": "https://t.co/8AS4wXwwUD",
+					"display_url": "pic.twitter.com/8AS4wXwwUD",
+					"expanded_url": "https://twitter.com/BDSdragonflies/status/1145779407744831488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1145779386618130400,
+					"id_str": "1145779386618130434",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D-agSBJWsAI3WPq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-agSBJWsAI3WPq.jpg",
+					"url": "https://t.co/8AS4wXwwUD",
+					"display_url": "pic.twitter.com/8AS4wXwwUD",
+					"expanded_url": "https://twitter.com/BDSdragonflies/status/1145779407744831488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 389414784,
+			"id_str": "389414784",
+			"name": "British Dragonfly Society",
+			"screen_name": "BDSdragonflies",
+			"location": "United Kingdom",
+			"description": "British Dragonfly Society, conserving dragonflies, damselflies & their wetland habitats since 1983. @ScotlandBds for news from Scotland Officers🏴󠁧󠁢󠁳󠁣󠁴󠁿🐉",
+			"url": "https://t.co/PVaYTfxAIg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PVaYTfxAIg",
+							"expanded_url": "http://www.british-dragonflies.org.uk",
+							"display_url": "british-dragonflies.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11487,
+			"friends_count": 489,
+			"listed_count": 171,
+			"created_at": "Wed Oct 12 11:34:11 +0000 2011",
+			"favourites_count": 7652,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10175,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1082591613438906369/xg10OEsA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1082591613438906369/xg10OEsA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/389414784/1561971236",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 33,
+		"favorite_count": 51,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🐉#DragonflyChallenge2019 🐉LEARN: FAQ we get ”How do you tell a Dragonfly from a Damselfly?\" If they fold wings down thier sides when perched you are watching a Damselfly. If they hold them out straight like the wings of an areoplane it’s a Dragonfly 🐉 https://t.co/tCQWl8kCrk https://t.co/8AS4wXwwUD"
+	},
+	{
 		"created_at": "Wed Jul 03 19:35:43 +0000 2019",
 		"id": 1146502801821962200,
 		"id_str": "1146502801821962241",
