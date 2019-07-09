c951b41fd911bf074e93caf9a1f338fbc5c8faa3
John Johnston <johnjohnston@mac.com>
2019-07-09 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17665,
-	"created_at": "Mon Jul 08 00:17:47 +0000 2019",
+	"tweets": 17666,
+	"created_at": "Tue Jul 09 00:17:47 +0000 2019",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2019_07.js
+++ tweets/data/js/tweets/2019_07.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Mon Jul 08 18:48:45 +0000 2019",
+		"id": 1148302921966280700,
+		"id_str": "1148302921966280705",
+		"full_text": "Ardinning 2019-07-08 late afternoon\nOvercast a shower just stopped as we arrived. Warm &amp; calm.\nMeadow sweet &amp; Valerian coming on strong.\nLots of ringlets\nA pair of Great crested grebes on the loch.\nLots of spotted orchids and a couple of butterfly orchids( lesser or greater?) https://t.co/eHwGxSmRUl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			284
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1148302894082510800,
+					"id_str": "1148302894082510848",
+					"indices": [
+						285,
+						308
+					],
+					"media_url": "http://pbs.twimg.com/media/D--XZfBXUAAwBnQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D--XZfBXUAAwBnQ.jpg",
+					"url": "https://t.co/eHwGxSmRUl",
+					"display_url": "pic.twitter.com/eHwGxSmRUl",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1148302921966280705/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 620,
+							"h": 279,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 620,
+							"h": 279,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 620,
+							"h": 279,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1148302894082510800,
+					"id_str": "1148302894082510848",
+					"indices": [
+						285,
+						308
+					],
+					"media_url": "http://pbs.twimg.com/media/D--XZfBXUAAwBnQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D--XZfBXUAAwBnQ.jpg",
+					"url": "https://t.co/eHwGxSmRUl",
+					"display_url": "pic.twitter.com/eHwGxSmRUl",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1148302921966280705/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 620,
+							"h": 279,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 620,
+							"h": 279,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 620,
+							"h": 279,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1148302919177052200,
+					"id_str": "1148302919177052160",
+					"indices": [
+						285,
+						308
+					],
+					"media_url": "http://pbs.twimg.com/media/D--Xa8gXoAAtmKN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D--Xa8gXoAAtmKN.jpg",
+					"url": "https://t.co/eHwGxSmRUl",
+					"display_url": "pic.twitter.com/eHwGxSmRUl",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1148302921966280705/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1240,
+							"h": 930,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://micro.blog/\" rel=\"nofollow\">Micro.blog</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3911,
+			"friends_count": 2556,
+			"listed_count": 274,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4728,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17616,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Ardinning 2019-07-08 late afternoon\nOvercast a shower just stopped as we arrived. Warm &amp; calm.\nMeadow sweet &amp; Valerian coming on strong.\nLots of ringlets\nA pair of Great crested grebes on the loch.\nLots of spotted orchids and a couple of butterfly orchids( lesser or greater?) https://t.co/eHwGxSmRUl"
+	},
+	{
 		"created_at": "Sun Jul 07 19:17:21 +0000 2019",
 		"id": 1147947732348428300,
 		"id_str": "1147947732348428288",
