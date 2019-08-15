0abbe67e926f481dd2cff22a7e242c971bea66ac
John Johnston <johnjohnston@mac.com>
2019-08-15 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17719,
-	"created_at": "Wed Aug 14 00:17:47 +0000 2019",
+	"tweets": 17720,
+	"created_at": "Thu Aug 15 00:17:47 +0000 2019",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2019_08.js
+++ tweets/data/js/tweets/2019_08.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Wed Aug 14 17:32:20 +0000 2019",
+		"id": 1161692042944307200,
+		"id_str": "1161692042944307200",
+		"full_text": "@BmuirComputing @bw_clark that looks like a treasure trove of micro:bit ideas, thanks. Back issues too:-)",
+		"truncated": false,
+		"display_text_range": [
+			26,
+			105
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BmuirComputing",
+					"name": "BoroughmuirComputing",
+					"id": 2906291523,
+					"id_str": "2906291523",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "bw_clark",
+					"name": "Brian Clark",
+					"id": 81107352,
+					"id_str": "81107352",
+					"indices": [
+						16,
+						25
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1160545542759493600,
+		"in_reply_to_status_id_str": "1160545542759493634",
+		"in_reply_to_user_id": 2906291523,
+		"in_reply_to_user_id_str": "2906291523",
+		"in_reply_to_screen_name": "BmuirComputing",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3914,
+			"friends_count": 2570,
+			"listed_count": 272,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4800,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17687,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@BmuirComputing @bw_clark that looks like a treasure trove of micro:bit ideas, thanks. Back issues too:-)"
+	},
+	{
 		"created_at": "Tue Aug 13 10:52:35 +0000 2019",
 		"id": 1161221524113842200,
 		"id_str": "1161229051702460416",
