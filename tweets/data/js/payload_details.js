5a5c72df552ebe049e13d6ee4fb4d6f8097a7663
John Johnston <johnjohnston@mac.com>
2019-08-25 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17728,
-	"created_at": "Sat Aug 24 00:17:47 +0000 2019",
+	"tweets": 17729,
+	"created_at": "Sun Aug 25 00:17:48 +0000 2019",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2019_08.js
+++ tweets/data/js/tweets/2019_08.js
@@ -1,6 +1,298 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 24 17:41:20 +0000 2019",
+		"id": 1165318186444710000,
+		"id_str": "1165318186444709889",
+		"full_text": "Loch Ardinning this afternoon. After quite a wet spell it is summerish again. Peacocks and the odd tortoiseshell, damselflies and dragonflies. https://t.co/yrGRGbi9Xm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1165318178664308700,
+					"id_str": "1165318178664308738",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/ECwKtZmXYAIKSM-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECwKtZmXYAIKSM-.jpg",
+					"url": "https://t.co/yrGRGbi9Xm",
+					"display_url": "pic.twitter.com/yrGRGbi9Xm",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1165318186444709889/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 620,
+							"h": 465,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 620,
+							"h": 465,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 620,
+							"h": 465,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1165318178664308700,
+					"id_str": "1165318178664308738",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/ECwKtZmXYAIKSM-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECwKtZmXYAIKSM-.jpg",
+					"url": "https://t.co/yrGRGbi9Xm",
+					"display_url": "pic.twitter.com/yrGRGbi9Xm",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1165318186444709889/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 620,
+							"h": 465,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 620,
+							"h": 465,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 620,
+							"h": 465,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1165318180719517700,
+					"id_str": "1165318180719517696",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/ECwKthQXYAAcvn2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECwKthQXYAAcvn2.jpg",
+					"url": "https://t.co/yrGRGbi9Xm",
+					"display_url": "pic.twitter.com/yrGRGbi9Xm",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1165318186444709889/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1165318182741139500,
+					"id_str": "1165318182741139466",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/ECwKtoyW4AoCEPl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECwKtoyW4AoCEPl.jpg",
+					"url": "https://t.co/yrGRGbi9Xm",
+					"display_url": "pic.twitter.com/yrGRGbi9Xm",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1165318186444709889/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1165318184796328000,
+					"id_str": "1165318184796327936",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/ECwKtwcWkAAoaCu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECwKtwcWkAAoaCu.jpg",
+					"url": "https://t.co/yrGRGbi9Xm",
+					"display_url": "pic.twitter.com/yrGRGbi9Xm",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1165318186444709889/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://micro.blog/\" rel=\"nofollow\">Micro.blog</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3915,
+			"friends_count": 2568,
+			"listed_count": 272,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4806,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17701,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Loch Ardinning this afternoon. After quite a wet spell it is summerish again. Peacocks and the odd tortoiseshell, damselflies and dragonflies. https://t.co/yrGRGbi9Xm"
+	},
+	{
 		"created_at": "Fri Aug 23 19:43:18 +0000 2019",
 		"id": 1164986490256052200,
 		"id_str": "1164986490256052224",
