cc53b58fceeb5234b49f725b30a6822dc89f3a76
John Johnston <johnjohnston@mac.com>
2019-08-31 01:17:48 +0100

    Added 2 tweets

--- tweets/data/js/tweets/2019_08.js
+++ tweets/data/js/tweets/2019_08.js
@@ -1,6 +1,450 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 30 18:03:55 +0000 2019",
+		"id": 1167498197146771500,
+		"id_str": "1167498197146771457",
+		"full_text": "@joecar You might want to add this to https://t.co/9p4Duh2jQO",
+		"truncated": false,
+		"display_text_range": [
+			8,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "joecar",
+					"name": "Joe Wilson",
+					"id": 10140072,
+					"id_str": "10140072",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/9p4Duh2jQO",
+					"expanded_url": "http://teachmeet.scot",
+					"display_url": "teachmeet.scot",
+					"indices": [
+						38,
+						61
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1167351723452747800,
+		"in_reply_to_status_id_str": "1167351723452747777",
+		"in_reply_to_user_id": 10140072,
+		"in_reply_to_user_id_str": "10140072",
+		"in_reply_to_screen_name": "joecar",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3917,
+			"friends_count": 2572,
+			"listed_count": 272,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4812,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17709,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "791e00bcadc4615f",
+			"url": "https://api.twitter.com/1.1/geo/id/791e00bcadc4615f.json",
+			"place_type": "city",
+			"name": "Glasgow",
+			"full_name": "Glasgow, Scotland",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-4.3932845,
+							55.796184
+						],
+						[
+							-4.0902182,
+							55.796184
+						],
+						[
+							-4.0902182,
+							55.9204214
+						],
+						[
+							-4.3932845,
+							55.9204214
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@joecar You might want to add this to https://t.co/9p4Duh2jQO"
+	},
+	{
+		"created_at": "Fri Aug 30 18:03:18 +0000 2019",
+		"id": 1167351723452747800,
+		"id_str": "1167498039168294914",
+		"full_text": "Come along and find out how you can engage with #NMIS https://t.co/tNv9dFo16m #digilearnscot #edtech https://t.co/zR2RFKvLlu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NMIS",
+					"indices": [
+						48,
+						53
+					]
+				},
+				{
+					"text": "digilearnscot",
+					"indices": [
+						78,
+						92
+					]
+				},
+				{
+					"text": "edtech",
+					"indices": [
+						93,
+						100
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tNv9dFo16m",
+					"expanded_url": "https://digit.fyi/advanced-manufacturing-innovation-district-work-begin",
+					"display_url": "digit.fyi/advanced-manuf…",
+					"indices": [
+						54,
+						77
+					]
+				},
+				{
+					"url": "https://t.co/zR2RFKvLlu",
+					"expanded_url": "https://twitter.com/nmis_skills/status/1167338412636090368",
+					"display_url": "twitter.com/nmis_skills/st…",
+					"indices": [
+						101,
+						124
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 10140072,
+			"id_str": "10140072",
+			"name": "Joe Wilson",
+			"screen_name": "joecar",
+			"location": "Glasgow, Scotland",
+			"description": "Consultant, Learning Technology, UK, International, Vocational Systems Delivery and Assessment  Own Opinions ! Currently @CofGCollege shaping learning futures",
+			"url": "https://t.co/SDsqUlqKvt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SDsqUlqKvt",
+							"expanded_url": "http://www.joewilsons.net/",
+							"display_url": "joewilsons.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4621,
+			"friends_count": 5060,
+			"listed_count": 298,
+			"created_at": "Sun Nov 11 00:14:00 +0000 2007",
+			"favourites_count": 4316,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19636,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1250766273/49942_672577162_4050981_q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1250766273/49942_672577162_4050981_q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/10140072/1396962370",
+			"profile_link_color": "FF691F",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1167338412636090400,
+		"quoted_status_id_str": "1167338412636090368",
+		"quoted_status_permalink": {
+			"url": "https://t.co/zR2RFKvLlu",
+			"expanded": "https://twitter.com/nmis_skills/status/1167338412636090368",
+			"display": "twitter.com/nmis_skills/st…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Aug 30 07:29:00 +0000 2019",
+			"id": 1167338412636090400,
+			"id_str": "1167338412636090368",
+			"full_text": "Join #nmis ambassadors and digital teaching experts at the #NMISSkills #teachmeet at Edinburgh College on Wednesday 2nd October 2019, 16.30-18.30 \nBook your place as a participant or as a presenter now! https://t.co/oPUVMdSwo1\n#digilearnscot #edtech",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				249
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "nmis",
+						"indices": [
+							5,
+							10
+						]
+					},
+					{
+						"text": "NMISSkills",
+						"indices": [
+							59,
+							70
+						]
+					},
+					{
+						"text": "teachmeet",
+						"indices": [
+							71,
+							81
+						]
+					},
+					{
+						"text": "digilearnscot",
+						"indices": [
+							227,
+							241
+						]
+					},
+					{
+						"text": "edtech",
+						"indices": [
+							242,
+							249
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/oPUVMdSwo1",
+						"expanded_url": "https://buff.ly/2ZoQr5h",
+						"display_url": "buff.ly/2ZoQr5h",
+						"indices": [
+							203,
+							226
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1100027597038202900,
+				"id_str": "1100027597038202880",
+				"name": "NMIS Skills",
+				"screen_name": "nmis_skills",
+				"location": "Scotland, United Kingdom",
+				"description": "News from the NMIS Skills Project",
+				"url": "https://t.co/64cbsh6zca",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/64cbsh6zca",
+								"expanded_url": "https://www.nmis-skills.org/",
+								"display_url": "nmis-skills.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 62,
+				"friends_count": 126,
+				"listed_count": 0,
+				"created_at": "Mon Feb 25 13:39:51 +0000 2019",
+				"favourites_count": 52,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 124,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1108704573810790400/ma9rYmch_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1108704573810790400/ma9rYmch_normal.png",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Come along and find out how you can engage with #NMIS https://t.co/tNv9dFo16m #digilearnscot #edtech https://t.co/zR2RFKvLlu"
+	},
+	{
 		"created_at": "Thu Aug 29 20:56:01 +0000 2019",
 		"id": 1167179117592293400,
 		"id_str": "1167179117592293376",
