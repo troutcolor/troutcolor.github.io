42623dc10605c9880fe3013d8b74de5a1afc87cf
John Johnston <johnjohnston@mac.com>
2019-09-13 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/tweets/2019_09.js
+++ tweets/data/js/tweets/2019_09.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Thu Sep 12 19:49:55 +0000 2019",
+		"id": 1172235914145337300,
+		"id_str": "1172235914145337346",
+		"full_text": "@BC_Scotland @BuglifeScotland @savebutterflies Thanks folk. Appreciate the help and link.",
+		"truncated": false,
+		"display_text_range": [
+			47,
+			89
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BC_Scotland",
+					"name": "BC Scotland",
+					"id": 3654010522,
+					"id_str": "3654010522",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "BuglifeScotland",
+					"name": "BuglifeScotland",
+					"id": 905051659465785300,
+					"id_str": "905051659465785344",
+					"indices": [
+						13,
+						29
+					]
+				},
+				{
+					"screen_name": "savebutterflies",
+					"name": "Butterfly Conservation",
+					"id": 257396212,
+					"id_str": "257396212",
+					"indices": [
+						30,
+						46
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1172148212574146600,
+		"in_reply_to_status_id_str": "1172148212574146561",
+		"in_reply_to_user_id": 3654010522,
+		"in_reply_to_user_id_str": "3654010522",
+		"in_reply_to_screen_name": "BC_Scotland",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3917,
+			"friends_count": 2572,
+			"listed_count": 272,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4834,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17722,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@BC_Scotland @BuglifeScotland @savebutterflies Thanks folk. Appreciate the help and link."
+	},
+	{
 		"created_at": "Wed Sep 11 20:21:44 +0000 2019",
 		"id": 1171381040897806300,
 		"id_str": "1171881531750699008",
