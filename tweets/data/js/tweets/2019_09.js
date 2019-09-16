a30bf9133bbd12301d3622af71b7796aca489853
John Johnston <johnjohnston@mac.com>
2019-09-16 01:17:49 +0100

    Added 1 tweets

--- tweets/data/js/tweets/2019_09.js
+++ tweets/data/js/tweets/2019_09.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sun Sep 15 15:43:26 +0000 2019",
+		"id": 1173261045437124600,
+		"id_str": "1173261045437124608",
+		"full_text": "Had a quick walk around the Kilpatrick moor to the Slacks Trigpoint and back. Overcast with a fairly strong breeze. Autumn seems to have started. Saw a couple of Red Admirals when the sun tried to get through. https://t.co/Pt2k4MJP2a",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			209
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1173261038097227800,
+					"id_str": "1173261038097227776",
+					"indices": [
+						210,
+						233
+					],
+					"media_url": "http://pbs.twimg.com/media/EEhCsqyXYAARhnT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EEhCsqyXYAARhnT.jpg",
+					"url": "https://t.co/Pt2k4MJP2a",
+					"display_url": "pic.twitter.com/Pt2k4MJP2a",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1173261045437124608/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1240,
+							"h": 1240,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1173261038097227800,
+					"id_str": "1173261038097227776",
+					"indices": [
+						210,
+						233
+					],
+					"media_url": "http://pbs.twimg.com/media/EEhCsqyXYAARhnT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EEhCsqyXYAARhnT.jpg",
+					"url": "https://t.co/Pt2k4MJP2a",
+					"display_url": "pic.twitter.com/Pt2k4MJP2a",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1173261045437124608/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1240,
+							"h": 1240,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1173261041184231400,
+					"id_str": "1173261041184231424",
+					"indices": [
+						210,
+						233
+					],
+					"media_url": "http://pbs.twimg.com/media/EEhCs2SXUAAC24n.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EEhCs2SXUAAC24n.jpg",
+					"url": "https://t.co/Pt2k4MJP2a",
+					"display_url": "pic.twitter.com/Pt2k4MJP2a",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1173261045437124608/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1240,
+							"h": 1240,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1173261043407171600,
+					"id_str": "1173261043407171584",
+					"indices": [
+						210,
+						233
+					],
+					"media_url": "http://pbs.twimg.com/media/EEhCs-kWsAAesNV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EEhCs-kWsAAesNV.jpg",
+					"url": "https://t.co/Pt2k4MJP2a",
+					"display_url": "pic.twitter.com/Pt2k4MJP2a",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1173261045437124608/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1240,
+							"h": 1240,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://micro.blog/\" rel=\"nofollow\">Micro.blog</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3917,
+			"friends_count": 2574,
+			"listed_count": 272,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4836,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17727,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Had a quick walk around the Kilpatrick moor to the Slacks Trigpoint and back. Overcast with a fairly strong breeze. Autumn seems to have started. Saw a couple of Red Admirals when the sun tried to get through. https://t.co/Pt2k4MJP2a"
+	},
+	{
 		"created_at": "Sat Sep 14 17:24:54 +0000 2019",
 		"id": 1172924193421308000,
 		"id_str": "1172924193421307905",
