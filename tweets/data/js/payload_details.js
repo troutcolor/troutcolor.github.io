d8db1b95c1d7b37b71de50fbea7ddac73377a469
John Johnston <johnjohnston@mac.com>
2019-10-16 01:17:50 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17781,
-	"created_at": "Tue Oct 15 00:17:48 +0000 2019",
+	"tweets": 17782,
+	"created_at": "Wed Oct 16 00:17:49 +0000 2019",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2019_10.js
+++ tweets/data/js/tweets/2019_10.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Tue Oct 15 19:55:28 +0000 2019",
+		"id": 1184196111034146800,
+		"id_str": "1184196111034146817",
+		"full_text": "@andrewkbailey13 @richardjholme @dundeeuni @BoddingtonsUK @drewfoster0 @darynsimon @MrEFinch @pedagoo @researchEDScot1 @IanStuart66 @magsamond has a ton of info on #teachmeet",
+		"truncated": false,
+		"display_text_range": [
+			132,
+			174
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "teachmeet",
+					"indices": [
+						164,
+						174
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "andrewkbailey13",
+					"name": "Andrew Bailey",
+					"id": 283494396,
+					"id_str": "283494396",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "richardjholme",
+					"name": "richard holme",
+					"id": 209598541,
+					"id_str": "209598541",
+					"indices": [
+						17,
+						31
+					]
+				},
+				{
+					"screen_name": "dundeeuni",
+					"name": "University of Dundee",
+					"id": 34934712,
+					"id_str": "34934712",
+					"indices": [
+						32,
+						42
+					]
+				},
+				{
+					"screen_name": "BoddingtonsUK",
+					"name": "Boddingtons | Official",
+					"id": 937645887899123700,
+					"id_str": "937645887899123713",
+					"indices": [
+						43,
+						57
+					]
+				},
+				{
+					"screen_name": "drewfoster0",
+					"name": "Maths-Beagle",
+					"id": 197885106,
+					"id_str": "197885106",
+					"indices": [
+						58,
+						70
+					]
+				},
+				{
+					"screen_name": "darynsimon",
+					"name": "Daryn Egan-Simon",
+					"id": 15931104,
+					"id_str": "15931104",
+					"indices": [
+						71,
+						82
+					]
+				},
+				{
+					"screen_name": "MrEFinch",
+					"name": "Ed Finch",
+					"id": 1921177688,
+					"id_str": "1921177688",
+					"indices": [
+						83,
+						92
+					]
+				},
+				{
+					"screen_name": "pedagoo",
+					"name": "Pedagoo.org",
+					"id": 291838882,
+					"id_str": "291838882",
+					"indices": [
+						93,
+						101
+					]
+				},
+				{
+					"screen_name": "researchEDScot1",
+					"name": "researchED Scotland",
+					"id": 1065244978383851500,
+					"id_str": "1065244978383851520",
+					"indices": [
+						102,
+						118
+					]
+				},
+				{
+					"screen_name": "IanStuart66",
+					"name": "Ian Stuart",
+					"id": 2228692152,
+					"id_str": "2228692152",
+					"indices": [
+						119,
+						131
+					]
+				},
+				{
+					"screen_name": "magsamond",
+					"name": "magsamond",
+					"id": 21595696,
+					"id_str": "21595696",
+					"indices": [
+						132,
+						142
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1184184053731344400,
+		"in_reply_to_status_id_str": "1184184053731344387",
+		"in_reply_to_user_id": 283494396,
+		"in_reply_to_user_id_str": "283494396",
+		"in_reply_to_screen_name": "andrewkbailey13",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3919,
+			"friends_count": 2577,
+			"listed_count": 273,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4869,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17774,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@andrewkbailey13 @richardjholme @dundeeuni @BoddingtonsUK @drewfoster0 @darynsimon @MrEFinch @pedagoo @researchEDScot1 @IanStuart66 @magsamond has a ton of info on #teachmeet"
+	},
+	{
 		"created_at": "Mon Oct 14 13:10:08 +0000 2019",
 		"id": 1183731716533424000,
 		"id_str": "1183731716533424128",
