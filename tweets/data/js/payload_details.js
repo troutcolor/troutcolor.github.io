5c45a0fec843d2438905ab3018652c7a4b357f8d
John Johnston <johnjohnston@mac.com>
2019-11-22 00:17:49 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17816,
-	"created_at": "Thu Nov 21 00:17:47 +0000 2019",
+	"tweets": 17817,
+	"created_at": "Fri Nov 22 00:17:48 +0000 2019",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2019_11.js
+++ tweets/data/js/tweets/2019_11.js
@@ -1,6 +1,335 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Thu Nov 21 07:48:22 +0000 2019",
+		"id": 1197421478033141800,
+		"id_str": "1197421478033141761",
+		"full_text": "Sunrise @Banton_Pr https://t.co/Plo9IjuMQ2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			18
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Banton_Pr",
+					"name": "Banton Primary",
+					"id": 702565959354163200,
+					"id_str": "702565959354163200",
+					"indices": [
+						8,
+						18
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1197421465513189400,
+					"id_str": "1197421465513189377",
+					"indices": [
+						19,
+						42
+					],
+					"media_url": "http://pbs.twimg.com/media/EJ4YeopXYAEeFqK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EJ4YeopXYAEeFqK.jpg",
+					"url": "https://t.co/Plo9IjuMQ2",
+					"display_url": "pic.twitter.com/Plo9IjuMQ2",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1197421478033141761/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 503,
+									"y": 374,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 1,
+									"y": 27,
+									"h": 49,
+									"w": 49
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 1516,
+									"y": 1128,
+									"h": 116,
+									"w": 116
+								},
+								{
+									"x": 4,
+									"y": 82,
+									"h": 150,
+									"w": 150
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 888,
+									"y": 660,
+									"h": 67,
+									"w": 67
+								},
+								{
+									"x": 2,
+									"y": 48,
+									"h": 87,
+									"w": 87
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1516,
+									"y": 1128,
+									"h": 116,
+									"w": 116
+								},
+								{
+									"x": 4,
+									"y": 82,
+									"h": 150,
+									"w": 150
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1197421465513189400,
+					"id_str": "1197421465513189377",
+					"indices": [
+						19,
+						42
+					],
+					"media_url": "http://pbs.twimg.com/media/EJ4YeopXYAEeFqK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EJ4YeopXYAEeFqK.jpg",
+					"url": "https://t.co/Plo9IjuMQ2",
+					"display_url": "pic.twitter.com/Plo9IjuMQ2",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1197421478033141761/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 503,
+									"y": 374,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 1,
+									"y": 27,
+									"h": 49,
+									"w": 49
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 1516,
+									"y": 1128,
+									"h": 116,
+									"w": 116
+								},
+								{
+									"x": 4,
+									"y": 82,
+									"h": 150,
+									"w": 150
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 888,
+									"y": 660,
+									"h": 67,
+									"w": 67
+								},
+								{
+									"x": 2,
+									"y": 48,
+									"h": 87,
+									"w": 87
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1516,
+									"y": 1128,
+									"h": 116,
+									"w": 116
+								},
+								{
+									"x": 4,
+									"y": 82,
+									"h": 150,
+									"w": 150
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3927,
+			"friends_count": 2585,
+			"listed_count": 273,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4884,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17824,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Sunrise @Banton_Pr https://t.co/Plo9IjuMQ2"
+	},
+	{
 		"created_at": "Wed Nov 20 19:53:31 +0000 2019",
 		"id": 1197241582174515200,
 		"id_str": "1197241582174515200",
