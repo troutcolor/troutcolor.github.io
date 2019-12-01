54ee62d9fde3a68eaf48a4a5e6222cb67fca4422
John Johnston <johnjohnston@mac.com>
2019-12-01 00:17:49 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17823,
-	"created_at": "Sat Nov 30 00:17:47 +0000 2019",
+	"tweets": 17824,
+	"created_at": "Sun Dec 01 00:17:48 +0000 2019",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2019_11.js
+++ tweets/data/js/tweets/2019_11.js
@@ -1,6 +1,239 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Sat Nov 30 20:24:55 +0000 2019",
+		"id": 1200873362458849300,
+		"id_str": "1200873362458849280",
+		"full_text": "Glen Fruin Loop: A beautiful walk today, over Beinn Chaorach, Beinn Tharsuinn and From Beinn a Mhanaich. Frost and a clear blue sky. Notes, pictures and map: https://t.co/Ygxp1SUoHj (https://t.co/P2AXVvpFJO) https://t.co/spJ824SAKz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			207
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Ygxp1SUoHj",
+					"expanded_url": "https://johnjohnston.info/walks/maps.html?f=72157712004070632&m=2019-11-30-Glen-Fruin-Loop.gpx",
+					"display_url": "johnjohnston.info/walks/maps.htm…",
+					"indices": [
+						158,
+						181
+					]
+				},
+				{
+					"url": "https://t.co/P2AXVvpFJO",
+					"expanded_url": "https://johnjohnston.info/blog/glen-fruin-loop/",
+					"display_url": "johnjohnston.info/blog/glen-frui…",
+					"indices": [
+						183,
+						206
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1200873360584036400,
+					"id_str": "1200873360584036353",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/EKpb9X3XUAEnHzA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKpb9X3XUAEnHzA.jpg",
+					"url": "https://t.co/spJ824SAKz",
+					"display_url": "pic.twitter.com/spJ824SAKz",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1200873362458849280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 668,
+							"h": 668,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 668,
+							"h": 668,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 668,
+							"h": 668,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1200873360584036400,
+					"id_str": "1200873360584036353",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/EKpb9X3XUAEnHzA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKpb9X3XUAEnHzA.jpg",
+					"url": "https://t.co/spJ824SAKz",
+					"display_url": "pic.twitter.com/spJ824SAKz",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1200873362458849280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 668,
+							"h": 668,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 668,
+							"h": 668,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 668,
+							"h": 668,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3933,
+			"friends_count": 2590,
+			"listed_count": 273,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4886,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17833,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Glen Fruin Loop: A beautiful walk today, over Beinn Chaorach, Beinn Tharsuinn and From Beinn a Mhanaich. Frost and a clear blue sky. Notes, pictures and map: https://t.co/Ygxp1SUoHj (https://t.co/P2AXVvpFJO) https://t.co/spJ824SAKz"
+	},
+	{
 		"created_at": "Fri Nov 29 17:55:12 +0000 2019",
 		"id": 1200473295717650400,
 		"id_str": "1200473295717650437",
