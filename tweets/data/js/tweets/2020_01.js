ae5b60add89b5909715b7fc62b4d400854c8d28b
John Johnston <johnjohnston@mac.com>
2020-01-19 00:17:51 +0000

    Added 1 tweets

--- tweets/data/js/tweets/2020_01.js
+++ tweets/data/js/tweets/2020_01.js
@@ -1,6 +1,280 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Sat Jan 18 18:18:44 +0000 2020",
+		"id": 1218598613414219800,
+		"id_str": "1218598613414219777",
+		"full_text": "At last a bright day. I've got a new camera, this was at 85x zoom (65 optical and the rest digital I guess). The zoom lens is a good way to watch at a distance. (https://t.co/55qQN6creN) https://t.co/rM8FnY8aPq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			186
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/55qQN6creN",
+					"expanded_url": "https://johnjohnston.info/blog/ravens/",
+					"display_url": "johnjohnston.info/blog/ravens/",
+					"indices": [
+						162,
+						185
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1218598604195147800,
+					"id_str": "1218598604195147776",
+					"indices": [
+						187,
+						210
+					],
+					"media_url": "http://pbs.twimg.com/media/EOlU-V7WsAA07mW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EOlU-V7WsAA07mW.jpg",
+					"url": "https://t.co/rM8FnY8aPq",
+					"display_url": "pic.twitter.com/rM8FnY8aPq",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1218598613414219777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 668,
+							"h": 501,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 668,
+							"h": 501,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 668,
+							"h": 501,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1218598604195147800,
+					"id_str": "1218598604195147776",
+					"indices": [
+						187,
+						210
+					],
+					"media_url": "http://pbs.twimg.com/media/EOlU-V7WsAA07mW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EOlU-V7WsAA07mW.jpg",
+					"url": "https://t.co/rM8FnY8aPq",
+					"display_url": "pic.twitter.com/rM8FnY8aPq",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1218598613414219777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 668,
+							"h": 501,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 668,
+							"h": 501,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 668,
+							"h": 501,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				},
+				{
+					"id": 1218598610759233500,
+					"id_str": "1218598610759233536",
+					"indices": [
+						187,
+						210
+					],
+					"media_url": "http://pbs.twimg.com/media/EOlU-uYWsAAx4fo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EOlU-uYWsAAx4fo.jpg",
+					"url": "https://t.co/rM8FnY8aPq",
+					"display_url": "pic.twitter.com/rM8FnY8aPq",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1218598613414219777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3937,
+			"friends_count": 2598,
+			"listed_count": 273,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4932,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17919,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "At last a bright day. I've got a new camera, this was at 85x zoom (65 optical and the rest digital I guess). The zoom lens is a good way to watch at a distance. (https://t.co/55qQN6creN) https://t.co/rM8FnY8aPq"
+	},
+	{
 		"created_at": "Fri Jan 17 20:35:13 +0000 2020",
 		"id": 1218270571307458600,
 		"id_str": "1218270571307458562",
