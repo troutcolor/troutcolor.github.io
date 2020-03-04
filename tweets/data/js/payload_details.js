580b39bef6d8f75aeb20a2e3465699f04243c94d
John Johnston <johnjohnston@mac.com>
2020-03-04 00:17:49 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17947,
-	"created_at": "Tue Mar 03 00:17:48 +0000 2020",
+	"tweets": 17948,
+	"created_at": "Wed Mar 04 00:17:48 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_03.js
+++ tweets/data/js/tweets/2020_03.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Tue Mar 03 08:21:37 +0000 2020",
+		"id": 1234755799530049500,
+		"id_str": "1234755799530049536",
+		"full_text": "@microbit_edu @Nominet The micro:bit looks as if it might be a really neat idea, local storage, no sign-in for teacher or pupils. Download everything and resume session later. I am wondering if it will, now or soon, work with the iOS micro:bit app? (https://t.co/g4pCiPKTHa)",
+		"truncated": false,
+		"display_text_range": [
+			23,
+			274
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "microbit_edu",
+					"name": "Micro:bit Educational Foundation",
+					"id": 763102211639537700,
+					"id_str": "763102211639537665",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "Nominet",
+					"name": "Nominet",
+					"id": 20052789,
+					"id_str": "20052789",
+					"indices": [
+						14,
+						22
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/g4pCiPKTHa",
+					"expanded_url": "https://johnjohnston.info/blog/11944-2/",
+					"display_url": "johnjohnston.info/blog/11944-2/",
+					"indices": [
+						250,
+						273
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": 1234527639043371000,
+		"in_reply_to_status_id_str": "1234527639043371008",
+		"in_reply_to_user_id": 763102211639537700,
+		"in_reply_to_user_id_str": "763102211639537665",
+		"in_reply_to_screen_name": "microbit_edu",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher Scotland, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								135,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3941,
+			"friends_count": 2604,
+			"listed_count": 272,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 4973,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17992,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@microbit_edu @Nominet The micro:bit looks as if it might be a really neat idea, local storage, no sign-in for teacher or pupils. Download everything and resume session later. I am wondering if it will, now or soon, work with the iOS micro:bit app? (https://t.co/g4pCiPKTHa)"
+	},
+	{
 		"created_at": "Mon Mar 02 08:17:40 +0000 2020",
 		"id": 1234392416800723000,
 		"id_str": "1234392416800722944",
