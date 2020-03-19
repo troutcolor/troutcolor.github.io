43f65802e3f9609c2bec94072559cfdf4b7cea63
John Johnston <johnjohnston@mac.com>
2020-03-19 00:17:50 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17975,
-	"created_at": "Wed Mar 18 00:17:48 +0000 2020",
+	"tweets": 17976,
+	"created_at": "Thu Mar 19 00:17:48 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_03.js
+++ tweets/data/js/tweets/2020_03.js
@@ -1,6 +1,196 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Wed Mar 18 09:08:26 +0000 2020",
+		"id": 1240189473004142600,
+		"id_str": "1240203398366072833",
+		"full_text": "For help with #glowblogs, visit https://t.co/yOFPxlD6yN  Lots of help sheets and advice on setting up and maintaining blogs &amp; profiles.  Any @NAC_Education staff requiring help with class blogs, please add me as an administrator &amp; I will help you as much as I can. https://t.co/IsUTPRZziH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "glowblogs",
+					"indices": [
+						14,
+						24
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NAC_Education",
+					"name": "North Ayrshire Education",
+					"id": 704989626583412700,
+					"id_str": "704989626583412737",
+					"indices": [
+						145,
+						159
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/yOFPxlD6yN",
+					"expanded_url": "https://blogs.glowscotland.org.uk/na/gw09leerosslyn/glow-2/blogs/",
+					"display_url": "blogs.glowscotland.org.uk/na/gw09leeross…",
+					"indices": [
+						32,
+						55
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1240189307190730800,
+					"id_str": "1240189307190730752",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/ETYJmqVXYAA2MQS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETYJmqVXYAA2MQS.jpg",
+					"url": "https://t.co/IsUTPRZziH",
+					"display_url": "pic.twitter.com/IsUTPRZziH",
+					"expanded_url": "https://twitter.com/RosJLee/status/1240189473004142597/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 905,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 848,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1240189307190730800,
+					"id_str": "1240189307190730752",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/ETYJmqVXYAA2MQS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETYJmqVXYAA2MQS.jpg",
+					"url": "https://t.co/IsUTPRZziH",
+					"display_url": "pic.twitter.com/IsUTPRZziH",
+					"expanded_url": "https://twitter.com/RosJLee/status/1240189473004142597/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 905,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 848,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4165588108,
+			"id_str": "4165588108",
+			"name": "Rosslyn Lee",
+			"screen_name": "RosJLee",
+			"location": "",
+			"description": "#AppleTeacher, #MIEExpert & #GoogleCE Level 2, #STEM Ambassador. Be curious!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 739,
+			"friends_count": 283,
+			"listed_count": 54,
+			"created_at": "Wed Nov 11 18:08:56 +0000 2015",
+			"favourites_count": 3022,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2708,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1149343416876261376/k3XEpnXc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1149343416876261376/k3XEpnXc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4165588108/1563015593",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "For help with #glowblogs, visit https://t.co/yOFPxlD6yN  Lots of help sheets and advice on setting up and maintaining blogs &amp; profiles.  Any @NAC_Education staff requiring help with class blogs, please add me as an administrator &amp; I will help you as much as I can. https://t.co/IsUTPRZziH"
+	},
+	{
 		"created_at": "Tue Mar 17 21:08:02 +0000 2020",
 		"id": 1240022101052076000,
 		"id_str": "1240022101052076034",
