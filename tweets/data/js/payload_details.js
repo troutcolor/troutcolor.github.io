4c94b87bc14704264aaf58468f8946185d2c49b6
John Johnston <johnjohnston@mac.com>
2020-03-31 01:17:50 +0100

    Added 2 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18006,
-	"created_at": "Mon Mar 30 00:17:48 +0000 2020",
+	"tweets": 18008,
+	"created_at": "Tue Mar 31 00:17:49 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_03.js
+++ tweets/data/js/tweets/2020_03.js
@@ -1,6 +1,307 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Mon Mar 30 19:01:39 +0000 2020",
+		"id": 1244701338996539400,
+		"id_str": "1244701338996539395",
+		"full_text": "@ayelearning Post a photo of the sea. Just one photo, no description. The goal is to flood the TL with positive photos instead of negativity.\nPlease copy the text and add a picture. https://t.co/spptIzzFX9",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			181
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ayelearning",
+					"name": "Bill Boyd",
+					"id": 17738779,
+					"id_str": "17738779",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1244701333535502300,
+					"id_str": "1244701333535502336",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/EUYRRKTWsAADu26.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUYRRKTWsAADu26.jpg",
+					"url": "https://t.co/spptIzzFX9",
+					"display_url": "pic.twitter.com/spptIzzFX9",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1244701338996539395/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1244701333535502300,
+					"id_str": "1244701333535502336",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/EUYRRKTWsAADu26.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUYRRKTWsAADu26.jpg",
+					"url": "https://t.co/spptIzzFX9",
+					"display_url": "pic.twitter.com/spptIzzFX9",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1244701338996539395/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1244292152013832200,
+		"in_reply_to_status_id_str": "1244292152013832193",
+		"in_reply_to_user_id": 17738779,
+		"in_reply_to_user_id_str": "17738779",
+		"in_reply_to_screen_name": "ayelearning",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3949,
+			"friends_count": 2613,
+			"listed_count": 272,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5032,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18068,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ayelearning Post a photo of the sea. Just one photo, no description. The goal is to flood the TL with positive photos instead of negativity.\nPlease copy the text and add a picture. https://t.co/spptIzzFX9"
+	},
+	{
+		"created_at": "Mon Mar 30 15:48:20 +0000 2020",
+		"id": 1244652464936288300,
+		"id_str": "1244652688702427137",
+		"full_text": "A bit of fun on the Biggies Blog, Movie emoji quiz every day this week. Please don't guess on twitter, leave your guess on the blog. Answers next day.\n\nhttps://t.co/HOTgi1Hv2v",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HOTgi1Hv2v",
+					"expanded_url": "https://blogs.glowscotland.org.uk/nl/bantonbiggies/portfolio/movie-emoji-quiz/",
+					"display_url": "blogs.glowscotland.org.uk/nl/bantonbiggi…",
+					"indices": [
+						152,
+						175
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 702565959354163200,
+			"id_str": "702565959354163200",
+			"name": "Banton Primary",
+			"screen_name": "Banton_Pr",
+			"location": "Banton, Scotland",
+			"description": "A Small Primary School in North Lanarkshire",
+			"url": "https://t.co/WNBQyt3iF5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WNBQyt3iF5",
+							"expanded_url": "https://blogs.glowscotland.org.uk/nl/banton/",
+							"display_url": "blogs.glowscotland.org.uk/nl/banton/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 354,
+			"friends_count": 73,
+			"listed_count": 8,
+			"created_at": "Wed Feb 24 18:49:12 +0000 2016",
+			"favourites_count": 569,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 894,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/702578403564773376/V51uWMrV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/702578403564773376/V51uWMrV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/702565959354163200/1456342883",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A bit of fun on the Biggies Blog, Movie emoji quiz every day this week. Please don't guess on twitter, leave your guess on the blog. Answers next day.\n\nhttps://t.co/HOTgi1Hv2v"
+	},
+	{
 		"created_at": "Sun Mar 29 18:54:41 +0000 2020",
 		"id": 1244337197916045300,
 		"id_str": "1244337197916045315",
