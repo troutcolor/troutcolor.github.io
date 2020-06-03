ac90afed0a53458ef778a4aacf46700ee7c3bd1c
John Johnston <johnjohnston@mac.com>
2020-06-03 01:17:49 +0100

    Added 2 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18265,
-	"created_at": "Tue Jun 02 00:17:48 +0000 2020",
+	"tweets": 18267,
+	"created_at": "Wed Jun 03 00:17:47 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_06.js
+++ tweets/data/js/tweets/2020_06.js
@@ -1,6 +1,423 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Tue Jun 02 09:21:52 +0000 2020",
+		"id": 1267748255217131500,
+		"id_str": "1267748255217131521",
+		"full_text": "Bookmarked: What If Web – My #pcPopUp2020 Presentation. #pcPopUp2020 was one of the many online things I'd normally have paid attention to that I've missed. Aaron, @mrkrndvs is using the Thread Reader App Micropub publisher which looks interesting too.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			252
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "pcPopUp2020",
+					"indices": [
+						29,
+						41
+					]
+				},
+				{
+					"text": "pcPopUp2020",
+					"indices": [
+						56,
+						68
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mrkrndvs",
+					"name": "Aaron Davis 🏘️",
+					"id": 372768752,
+					"id_str": "372768752",
+					"indices": [
+						164,
+						173
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3947,
+			"friends_count": 2636,
+			"listed_count": 271,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5365,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18385,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "Bookmarked: What If Web – My #pcPopUp2020 Presentation. #pcPopUp2020 was one of the many online things I'd normally have paid attention to that I've missed. Aaron, @mrkrndvs is using the Thread Reader App Micropub publisher which looks interesting too."
+	},
+	{
+		"created_at": "Tue Jun 02 09:06:40 +0000 2020",
+		"id": 1267691676769947600,
+		"id_str": "1267744429529497600",
+		"full_text": "This https://t.co/IiqDqECuJQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			4
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IiqDqECuJQ",
+					"expanded_url": "https://twitter.com/Microsoft/status/1267580435313446918",
+					"display_url": "twitter.com/Microsoft/stat…",
+					"indices": [
+						5,
+						28
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2228692152,
+			"id_str": "2228692152",
+			"name": "Ian Stuart",
+			"screen_name": "IanStuart66",
+			"location": "Edinburgh, Scotland",
+			"description": "Formerly an Engineer, then an Educator, Consultant. Now with Microsoft Education. I am a learning & Teaching Nerd who uses technology and Design Thinking.",
+			"url": "https://t.co/kZhr18dawo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kZhr18dawo",
+							"expanded_url": "https://about.me/IslayIan",
+							"display_url": "about.me/IslayIan",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5735,
+			"friends_count": 5437,
+			"listed_count": 436,
+			"created_at": "Tue Dec 03 18:38:41 +0000 2013",
+			"favourites_count": 2153,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 46286,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/777067782852710400/0H-hfeZU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/777067782852710400/0H-hfeZU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2228692152/1386114209",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1267580435313447000,
+		"quoted_status_id_str": "1267580435313446918",
+		"quoted_status_permalink": {
+			"url": "https://t.co/IiqDqECuJQ",
+			"expanded": "https://twitter.com/Microsoft/status/1267580435313446918",
+			"display": "twitter.com/Microsoft/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jun 01 22:15:00 +0000 2020",
+			"id": 1267580435313447000,
+			"id_str": "1267580435313446918",
+			"full_text": "At this time, we will be using our platform to amplify voices from the Black and African American community at Microsoft. https://t.co/tMXGDTKoWv",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				121
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1267580432960360400,
+						"id_str": "1267580432960360448",
+						"indices": [
+							122,
+							145
+						],
+						"media_url": "http://pbs.twimg.com/media/EZdZsBbWkAA084p.png",
+						"media_url_https": "https://pbs.twimg.com/media/EZdZsBbWkAA084p.png",
+						"url": "https://t.co/tMXGDTKoWv",
+						"display_url": "pic.twitter.com/tMXGDTKoWv",
+						"expanded_url": "https://twitter.com/Microsoft/status/1267580435313446918/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 960,
+								"h": 540,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 960,
+								"h": 540,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1267580432960360400,
+						"id_str": "1267580432960360448",
+						"indices": [
+							122,
+							145
+						],
+						"media_url": "http://pbs.twimg.com/media/EZdZsBbWkAA084p.png",
+						"media_url_https": "https://pbs.twimg.com/media/EZdZsBbWkAA084p.png",
+						"url": "https://t.co/tMXGDTKoWv",
+						"display_url": "pic.twitter.com/tMXGDTKoWv",
+						"expanded_url": "https://twitter.com/Microsoft/status/1267580435313446918/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 960,
+								"h": 540,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 960,
+								"h": 540,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://prod2.sprinklr.com\" rel=\"nofollow\">Sprinklr Publisher</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 74286565,
+				"id_str": "74286565",
+				"name": "Microsoft",
+				"screen_name": "Microsoft",
+				"location": "Redmond, WA",
+				"description": "We’re on a mission to empower every person and every organization on the planet to achieve more. Support: @MicrosoftHelps",
+				"url": "https://t.co/bKBtZeAFrh",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/bKBtZeAFrh",
+								"expanded_url": "https://news.microsoft.com/",
+								"display_url": "news.microsoft.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 8943122,
+				"friends_count": 2558,
+				"listed_count": 23182,
+				"created_at": "Mon Sep 14 22:35:42 +0000 2009",
+				"favourites_count": 2838,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 17442,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "00AEEF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1267832826839228421/vAuwiWce_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267832826839228421/vAuwiWce_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/74286565/1590506774",
+				"profile_link_color": "1570A6",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 864,
+			"favorite_count": 3425,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This https://t.co/IiqDqECuJQ"
+	},
+	{
 		"created_at": "Mon Jun 01 09:24:54 +0000 2020",
 		"id": 1267386631088193500,
 		"id_str": "1267386631088193537",
