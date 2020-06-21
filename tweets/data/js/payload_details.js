f0a27d9a98df84228a2f0cace028d53d3fbd0bcb
John Johnston <johnjohnston@mac.com>
2020-06-21 01:17:49 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18312,
-	"created_at": "Sat Jun 20 00:17:48 +0000 2020",
+	"tweets": 18313,
+	"created_at": "Sun Jun 21 00:17:48 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_06.js
+++ tweets/data/js/tweets/2020_06.js
@@ -1,6 +1,200 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sat Jun 20 16:23:55 +0000 2020",
+		"id": 1274377449162375200,
+		"id_str": "1274377449162375175",
+		"full_text": "In normal times I listen to podcasts. As I do this on a commute I’ve only listened to 2 episodes since schools shut. I miss it.\n\nWhen listening I often want to remember something for later, perhaps for a blog post or like, usually this ends in… https://t.co/ssDmvM4ccy https://t.co/45WHhlDgM1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			268
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ssDmvM4ccy",
+					"expanded_url": "https://johnjohnston.info/blog/podcast-listening-notes/",
+					"display_url": "johnjohnston.info/blog/podcast-l…",
+					"indices": [
+						245,
+						268
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1274377446763217000,
+					"id_str": "1274377446763216897",
+					"indices": [
+						269,
+						292
+					],
+					"media_url": "http://pbs.twimg.com/media/Ea9_iTAXQAEoDSp.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ea9_iTAXQAEoDSp.png",
+					"url": "https://t.co/45WHhlDgM1",
+					"display_url": "pic.twitter.com/45WHhlDgM1",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1274377449162375175/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 668,
+							"h": 645,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 668,
+							"h": 645,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 668,
+							"h": 645,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1274377446763217000,
+					"id_str": "1274377446763216897",
+					"indices": [
+						269,
+						292
+					],
+					"media_url": "http://pbs.twimg.com/media/Ea9_iTAXQAEoDSp.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ea9_iTAXQAEoDSp.png",
+					"url": "https://t.co/45WHhlDgM1",
+					"display_url": "pic.twitter.com/45WHhlDgM1",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1274377449162375175/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 668,
+							"h": 645,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 668,
+							"h": 645,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 668,
+							"h": 645,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3940,
+			"friends_count": 2637,
+			"listed_count": 271,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5430,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18442,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In normal times I listen to podcasts. As I do this on a commute I’ve only listened to 2 episodes since schools shut. I miss it.\n\nWhen listening I often want to remember something for later, perhaps for a blog post or like, usually this ends in… https://t.co/ssDmvM4ccy https://t.co/45WHhlDgM1"
+	},
+	{
 		"created_at": "Fri Jun 19 09:08:28 +0000 2020",
 		"id": 1273905475873054700,
 		"id_str": "1273905475873054721",
