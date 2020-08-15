8eac242413b40f4235cd61c5b6306655bcf2aaaa
John Johnston <johnjohnston@mac.com>
2020-08-15 01:17:52 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18410,
-	"created_at": "Thu Aug 13 00:17:48 +0000 2020",
+	"tweets": 18411,
+	"created_at": "Sat Aug 15 00:17:51 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_08.js
+++ tweets/data/js/tweets/2020_08.js
@@ -1,6 +1,278 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Fri Aug 14 16:27:06 +0000 2020",
+		"id": 1294309584182620200,
+		"id_str": "1294309584182620162",
+		"full_text": "Brilliant https://t.co/9zarssuK4F",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			9
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9zarssuK4F",
+					"expanded_url": "https://twitter.com/digitalkatie/status/1294220354404089856",
+					"display_url": "twitter.com/digitalkatie/s…",
+					"indices": [
+						10,
+						33
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3938,
+			"friends_count": 2654,
+			"listed_count": 266,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5569,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18570,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "791e00bcadc4615f",
+			"url": "https://api.twitter.com/1.1/geo/id/791e00bcadc4615f.json",
+			"place_type": "city",
+			"name": "Glasgow",
+			"full_name": "Glasgow, Scotland",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-4.3932845,
+							55.796184
+						],
+						[
+							-4.0902182,
+							55.796184
+						],
+						[
+							-4.0902182,
+							55.9204214
+						],
+						[
+							-4.3932845,
+							55.9204214
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1294220354404089900,
+		"quoted_status_id_str": "1294220354404089856",
+		"quoted_status_permalink": {
+			"url": "https://t.co/9zarssuK4F",
+			"expanded": "https://twitter.com/digitalkatie/status/1294220354404089856",
+			"display": "twitter.com/digitalkatie/s…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Aug 14 10:32:32 +0000 2020",
+			"id": 1294220354404089900,
+			"id_str": "1294220354404089856",
+			"full_text": "Teens have figured out how to mess with Instagram's tracking algorithm https://t.co/AhIIZxt5ZF via @CNET",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				104
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "CNET",
+						"name": "CNET",
+						"id": 30261067,
+						"id_str": "30261067",
+						"indices": [
+							99,
+							104
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/AhIIZxt5ZF",
+						"expanded_url": "https://cnet.co/2vP26MR",
+						"display_url": "cnet.co/2vP26MR",
+						"indices": [
+							71,
+							94
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 8177542,
+				"id_str": "8177542",
+				"name": "Kate Farrell",
+				"screen_name": "digitalkatie",
+				"location": "Edinburgh",
+				"description": "CS educator working at Edinburgh Uni supporting schools with Data Science. British Sign Language Level 6 🤟 I enjoy playing board games and solving escape rooms",
+				"url": "https://t.co/XLCOW9s0oU",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/XLCOW9s0oU",
+								"expanded_url": "http://TeachCS.Scot",
+								"display_url": "TeachCS.Scot",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2445,
+				"friends_count": 2406,
+				"listed_count": 151,
+				"created_at": "Tue Aug 14 08:19:01 +0000 2007",
+				"favourites_count": 10051,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 12862,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "709397",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/750618989936664576/DoiGeePx_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/750618989936664576/DoiGeePx_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/8177542/1584782085",
+				"profile_link_color": "981CEB",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "A0C5C7",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Brilliant https://t.co/9zarssuK4F"
+	},
+	{
 		"created_at": "Wed Aug 12 20:49:35 +0000 2020",
 		"id": 1293650862406094800,
 		"id_str": "1293650862406094848",
