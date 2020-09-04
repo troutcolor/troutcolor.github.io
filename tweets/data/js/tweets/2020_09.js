ddb156b0628c7de432b9fb2bd85335226ccc215f
John Johnston <johnjohnston@mac.com>
2020-09-04 01:17:49 +0100

    Added 1 tweets

--- tweets/data/js/tweets/2020_09.js
+++ tweets/data/js/tweets/2020_09.js
@@ -1,6 +1,292 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Thu Sep 03 21:22:55 +0000 2020",
+		"id": 1301504911977390000,
+		"id_str": "1301631786930315265",
+		"full_text": "Tomorrow at 12 @dundeeuni Bookclub Podcast\n\n@UoDCulture @onedundee @UoD_ESW @STEMglasgow @STEMedscot @TesScotland @edublether @EDUtalkr \n\n#edutwitter #uodedu #gamesbasedlearning https://t.co/C03TAVjzwS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			177
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "edutwitter",
+					"indices": [
+						138,
+						149
+					]
+				},
+				{
+					"text": "uodedu",
+					"indices": [
+						150,
+						157
+					]
+				},
+				{
+					"text": "gamesbasedlearning",
+					"indices": [
+						158,
+						177
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dundeeuni",
+					"name": "University of Dundee",
+					"id": 34934712,
+					"id_str": "34934712",
+					"indices": [
+						15,
+						25
+					]
+				},
+				{
+					"screen_name": "UoDCulture",
+					"name": "Culture, University of Dundee",
+					"id": 1252555141389131800,
+					"id_str": "1252555141389131776",
+					"indices": [
+						44,
+						55
+					]
+				},
+				{
+					"screen_name": "onedundee",
+					"name": "One Dundee, UoD",
+					"id": 2965750119,
+					"id_str": "2965750119",
+					"indices": [
+						56,
+						66
+					]
+				},
+				{
+					"screen_name": "UoD_ESW",
+					"name": "ESW Univ of Dundee",
+					"id": 918042557069873200,
+					"id_str": "918042557069873152",
+					"indices": [
+						67,
+						75
+					]
+				},
+				{
+					"screen_name": "STEMglasgow",
+					"name": "STEM Glasgow",
+					"id": 835100319080198100,
+					"id_str": "835100319080198144",
+					"indices": [
+						76,
+						88
+					]
+				},
+				{
+					"screen_name": "STEMedscot",
+					"name": "STEM @ Education Scotland",
+					"id": 1101170831441432600,
+					"id_str": "1101170831441432580",
+					"indices": [
+						89,
+						100
+					]
+				},
+				{
+					"screen_name": "TesScotland",
+					"name": "Tes Scotland",
+					"id": 106125898,
+					"id_str": "106125898",
+					"indices": [
+						101,
+						113
+					]
+				},
+				{
+					"screen_name": "edublether",
+					"name": "EduBlether",
+					"id": 893491765369548800,
+					"id_str": "893491765369548804",
+					"indices": [
+						114,
+						125
+					]
+				},
+				{
+					"screen_name": "EDUtalkr",
+					"name": "Radio EDUtalk",
+					"id": 77933462,
+					"id_str": "77933462",
+					"indices": [
+						126,
+						135
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1301504417896837000,
+					"id_str": "1301504417896837122",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/Eg_fX1ZXsAIIdLo.png",
+					"media_url_https": "https://pbs.twimg.com/media/Eg_fX1ZXsAIIdLo.png",
+					"url": "https://t.co/C03TAVjzwS",
+					"display_url": "pic.twitter.com/C03TAVjzwS",
+					"expanded_url": "https://twitter.com/Jonathan_B_82/status/1301504911977390081/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 600,
+							"h": 335,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 600,
+							"h": 335,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 335,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1301504417896837000,
+					"id_str": "1301504417896837122",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/Eg_fX1ZXsAIIdLo.png",
+					"media_url_https": "https://pbs.twimg.com/media/Eg_fX1ZXsAIIdLo.png",
+					"url": "https://t.co/C03TAVjzwS",
+					"display_url": "pic.twitter.com/C03TAVjzwS",
+					"expanded_url": "https://twitter.com/Jonathan_B_82/status/1301504911977390081/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 600,
+							"h": 335,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 600,
+							"h": 335,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 335,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1245723770335232000,
+			"id_str": "1245723770335232000",
+			"name": "Jonathan Brown",
+			"screen_name": "Jonathan_B_82",
+			"location": "",
+			"description": "Husband, Dad, Lecturer at the University of Dundee, creator of The Family Learning App, aspiring writer!",
+			"url": "https://t.co/0WYHhQIzB2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0WYHhQIzB2",
+							"expanded_url": "http://www.thefamilylearningapp.com",
+							"display_url": "thefamilylearningapp.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 403,
+			"friends_count": 554,
+			"listed_count": 3,
+			"created_at": "Thu Apr 02 14:44:49 +0000 2020",
+			"favourites_count": 763,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 548,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1245724783708438528/OoohDqaZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1245724783708438528/OoohDqaZ_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Tomorrow at 12 @dundeeuni Bookclub Podcast\n\n@UoDCulture @onedundee @UoD_ESW @STEMglasgow @STEMedscot @TesScotland @edublether @EDUtalkr \n\n#edutwitter #uodedu #gamesbasedlearning https://t.co/C03TAVjzwS"
+	},
+	{
 		"created_at": "Wed Sep 02 17:40:29 +0000 2020",
 		"id": 1300895039585955800,
 		"id_str": "1301213420621107207",
