166a197efc25f76f1dda866aa6cd80dd6210ef20
John Johnston <johnjohnston@mac.com>
2020-09-16 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18451,
-	"created_at": "Tue Sep 15 00:17:48 +0000 2020",
+	"tweets": 18452,
+	"created_at": "Wed Sep 16 00:17:47 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_09.js
+++ tweets/data/js/tweets/2020_09.js
@@ -1,6 +1,153 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Tue Sep 15 18:53:37 +0000 2020",
+		"id": 1305942869400850400,
+		"id_str": "1305942869400850437",
+		"full_text": "Henry Hepburn,  @Henry_Hepburn &amp; Emma Seith, @Emma_Seith interview Blair Minchin  (@Mr_Minchin). Interesting opinions on sharing, returning to school during covid and more. I imagine Blair’s class go home exhausted every day. (https://t.co/wDwbZG0meh)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			255
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Henry_Hepburn",
+					"name": "Henry Hepburn",
+					"id": 593054821,
+					"id_str": "593054821",
+					"indices": [
+						16,
+						30
+					]
+				},
+				{
+					"screen_name": "Emma_Seith",
+					"name": "Emma Seith",
+					"id": 743032790019608600,
+					"id_str": "743032790019608576",
+					"indices": [
+						49,
+						60
+					]
+				},
+				{
+					"screen_name": "Mr_Minchin",
+					"name": "Blair Minchin",
+					"id": 225064064,
+					"id_str": "225064064",
+					"indices": [
+						87,
+						98
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/wDwbZG0meh",
+					"expanded_url": "https://johnjohnston.info/blog/listened-teachers-have-a-duty-to-share-their-great-ideas/",
+					"display_url": "johnjohnston.info/blog/listened-…",
+					"indices": [
+						231,
+						254
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3940,
+			"friends_count": 2663,
+			"listed_count": 267,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5604,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18624,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Henry Hepburn,  @Henry_Hepburn &amp; Emma Seith, @Emma_Seith interview Blair Minchin  (@Mr_Minchin). Interesting opinions on sharing, returning to school during covid and more. I imagine Blair’s class go home exhausted every day. (https://t.co/wDwbZG0meh)"
+	},
+	{
 		"created_at": "Mon Sep 14 21:11:30 +0000 2020",
 		"id": 1305615178764087300,
 		"id_str": "1305615178764087301",
@@ -175,7 +322,7 @@ Grailbird.data.tweets_2020_09 =
 				},
 				{
 					"screen_name": "cgerrard02",
-					"name": "Chris Gerrard #MIEExpert 🦸‍♂️",
+					"name": "Chris Gerrard #MIEExpert 🦸♂️",
 					"id": 819162812278800400,
 					"id_str": "819162812278800385",
 					"indices": [
@@ -275,7 +422,7 @@ Grailbird.data.tweets_2020_09 =
 				},
 				{
 					"screen_name": "DonnaMShah",
-					"name": "Donna Shah 🙋🏻‍♀️ #TVTTagTeam",
+					"name": "Donna Shah 🙋🏻♀️ #TVTTagTeam",
 					"id": 2527416623,
 					"id_str": "2527416623",
 					"indices": [
