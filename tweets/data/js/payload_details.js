5c196db0492103797b181c1f3352c1ecadec77f7
John Johnston <johnjohnston@mac.com>
2020-10-26 00:17:49 +0000

    Added 2 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18529,
-	"created_at": "Sun Oct 25 00:17:48 +0000 2020",
+	"tweets": 18531,
+	"created_at": "Mon Oct 26 00:17:48 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_10.js
+++ tweets/data/js/tweets/2020_10.js
@@ -1,6 +1,342 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Sun Oct 25 22:27:43 +0000 2020",
+		"id": 1320492262623760400,
+		"id_str": "1320492262623760385",
+		"full_text": "@TeachArtDesign @lenabellina @teachgratitude1 @Ethical_Leader Your posts are syndicating now:\nhttps://t.co/YlTBsHisHi\nThanks for joining in.\nApologies for the delay  I don't seem to be getting email alerts when blogs are added to the form. Email with more details tomorrow.",
+		"truncated": false,
+		"display_text_range": [
+			62,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TeachArtDesign",
+					"name": "F Leadbeater",
+					"id": 1005041659,
+					"id_str": "1005041659",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "lenabellina",
+					"name": "lenabellina #ubuntu",
+					"id": 30729815,
+					"id_str": "30729815",
+					"indices": [
+						16,
+						28
+					]
+				},
+				{
+					"screen_name": "teachgratitude1",
+					"name": "Jamie Thom",
+					"id": 765627028498485200,
+					"id_str": "765627028498485248",
+					"indices": [
+						29,
+						45
+					]
+				},
+				{
+					"screen_name": "Ethical_Leader",
+					"name": "Hannah Wilson #Ubuntu",
+					"id": 365627981,
+					"id_str": "365627981",
+					"indices": [
+						46,
+						61
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/YlTBsHisHi",
+					"expanded_url": "http://scotedublogs.org",
+					"display_url": "scotedublogs.org",
+					"indices": [
+						94,
+						117
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1320470170792505300,
+		"in_reply_to_status_id_str": "1320470170792505344",
+		"in_reply_to_user_id": 1005041659,
+		"in_reply_to_user_id_str": "1005041659",
+		"in_reply_to_screen_name": "TeachArtDesign",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3939,
+			"friends_count": 2668,
+			"listed_count": 267,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5658,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18712,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@TeachArtDesign @lenabellina @teachgratitude1 @Ethical_Leader Your posts are syndicating now:\nhttps://t.co/YlTBsHisHi\nThanks for joining in.\nApologies for the delay  I don't seem to be getting email alerts when blogs are added to the form. Email with more details tomorrow."
+	},
+	{
+		"created_at": "Sun Oct 25 22:05:51 +0000 2020",
+		"id": 1320486757587161000,
+		"id_str": "1320486757587161091",
+		"full_text": "@TeachArtDesign @lenabellina @teachgratitude1 @Ethical_Leader I think you have, emails are not being sent. I’ll sort it out.",
+		"truncated": false,
+		"display_text_range": [
+			62,
+			124
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TeachArtDesign",
+					"name": "F Leadbeater",
+					"id": 1005041659,
+					"id_str": "1005041659",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "lenabellina",
+					"name": "lenabellina #ubuntu",
+					"id": 30729815,
+					"id_str": "30729815",
+					"indices": [
+						16,
+						28
+					]
+				},
+				{
+					"screen_name": "teachgratitude1",
+					"name": "Jamie Thom",
+					"id": 765627028498485200,
+					"id_str": "765627028498485248",
+					"indices": [
+						29,
+						45
+					]
+				},
+				{
+					"screen_name": "Ethical_Leader",
+					"name": "Hannah Wilson #Ubuntu",
+					"id": 365627981,
+					"id_str": "365627981",
+					"indices": [
+						46,
+						61
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1320470170792505300,
+		"in_reply_to_status_id_str": "1320470170792505344",
+		"in_reply_to_user_id": 1005041659,
+		"in_reply_to_user_id_str": "1005041659",
+		"in_reply_to_screen_name": "TeachArtDesign",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3939,
+			"friends_count": 2668,
+			"listed_count": 267,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5658,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18712,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "791e00bcadc4615f",
+			"url": "https://api.twitter.com/1.1/geo/id/791e00bcadc4615f.json",
+			"place_type": "city",
+			"name": "Glasgow",
+			"full_name": "Glasgow, Scotland",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-4.3932845,
+							55.796184
+						],
+						[
+							-4.0902182,
+							55.796184
+						],
+						[
+							-4.0902182,
+							55.9204214
+						],
+						[
+							-4.3932845,
+							55.9204214
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@TeachArtDesign @lenabellina @teachgratitude1 @Ethical_Leader I think you have, emails are not being sent. I’ll sort it out."
+	},
+	{
 		"created_at": "Sat Oct 24 17:35:02 +0000 2020",
 		"id": 1320056218753249300,
 		"id_str": "1320056218753249280",
