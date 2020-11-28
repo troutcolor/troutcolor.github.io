85f8a9582153d637ba81dac8f57fce8fb531e98f
John Johnston <johnjohnston@mac.com>
2020-11-28 00:17:49 +0000

    Added 2 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18559,
-	"created_at": "Thu Nov 26 00:17:48 +0000 2020",
+	"tweets": 18561,
+	"created_at": "Sat Nov 28 00:17:48 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_11.js
+++ tweets/data/js/tweets/2020_11.js
@@ -1,6 +1,306 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Fri Nov 27 19:57:47 +0000 2020",
+		"id": 1332283641913872400,
+		"id_str": "1332413328833077248",
+		"full_text": "We want everyone living with dementia to benefit from the power of personal music. 🌍\n\nOur free materials are now available in Chinese, Gaelic, Hindi, Urdu &amp; Welsh. \n\nPlease RT and help us share the power of personal playlists for dementia. \n▶️https://t.co/3jReTryWlE https://t.co/HOF1hebJyy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3jReTryWlE",
+					"expanded_url": "https://www.playlistforlife.org.uk/resources/",
+					"display_url": "playlistforlife.org.uk/resources/",
+					"indices": [
+						247,
+						270
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1332283638575222800,
+					"id_str": "1332283638575222785",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/En046RlXUAE46OJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/En046RlXUAE46OJ.jpg",
+					"url": "https://t.co/HOF1hebJyy",
+					"display_url": "pic.twitter.com/HOF1hebJyy",
+					"expanded_url": "https://twitter.com/PlaylistForLife/status/1332283641913872385/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1332283638575222800,
+					"id_str": "1332283638575222785",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/En046RlXUAE46OJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/En046RlXUAE46OJ.jpg",
+					"url": "https://t.co/HOF1hebJyy",
+					"display_url": "pic.twitter.com/HOF1hebJyy",
+					"expanded_url": "https://twitter.com/PlaylistForLife/status/1332283641913872385/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2203221589,
+			"id_str": "2203221589",
+			"name": "Playlist for Life",
+			"screen_name": "PlaylistForLife",
+			"location": "Registered charity: SC044072",
+			"description": "Sharing the power of personal music for people living with dementia.💚🎵 \nStart building a personal playlist for yourself or a loved one today.",
+			"url": "https://t.co/qWCgqvEPgZ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qWCgqvEPgZ",
+							"expanded_url": "http://www.playlistforlife.org.uk/",
+							"display_url": "playlistforlife.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5436,
+			"friends_count": 740,
+			"listed_count": 77,
+			"created_at": "Tue Nov 19 14:41:47 +0000 2013",
+			"favourites_count": 2988,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6118,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "3DA165",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1231918057238487041/35NDLHDx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1231918057238487041/35NDLHDx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2203221589/1604312823",
+			"profile_link_color": "3DA165",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We want everyone living with dementia to benefit from the power of personal music. 🌍\n\nOur free materials are now available in Chinese, Gaelic, Hindi, Urdu &amp; Welsh. \n\nPlease RT and help us share the power of personal playlists for dementia. \n▶️https://t.co/3jReTryWlE https://t.co/HOF1hebJyy"
+	},
+	{
+		"created_at": "Fri Nov 27 18:49:23 +0000 2020",
+		"id": 1332396117930160000,
+		"id_str": "1332396117930160128",
+		"full_text": "Re: decode https://t.co/93DpbIYmC1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			34
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/93DpbIYmC1",
+					"expanded_url": "https://johnjohnston.info/blog/re-decode/",
+					"display_url": "johnjohnston.info/blog/re-decode/",
+					"indices": [
+						11,
+						34
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://publicize.wp.com/\" rel=\"nofollow\">WordPress.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3935,
+			"friends_count": 2668,
+			"listed_count": 267,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5718,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18757,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Re: decode https://t.co/93DpbIYmC1"
+	},
+	{
 		"created_at": "Wed Nov 25 20:25:56 +0000 2020",
 		"id": 1331695639525281800,
 		"id_str": "1331695639525281792",
