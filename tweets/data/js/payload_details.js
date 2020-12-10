6f8ff9e3c4709eaa21761ba52cde733b650fcf49
John Johnston <johnjohnston@mac.com>
2020-12-10 00:17:49 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18579,
-	"created_at": "Wed Dec 09 00:17:48 +0000 2020",
+	"tweets": 18580,
+	"created_at": "Thu Dec 10 00:17:47 +0000 2020",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2020_12.js
+++ tweets/data/js/tweets/2020_12.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Wed Dec 09 20:56:10 +0000 2020",
+		"id": 1336776675363860500,
+		"id_str": "1336776675363860481",
+		"full_text": "@ds106dc #tdc3254 #ds106 there is a lovely poem about putting things on a table:\nhttps://t.co/nFIp498rq1\n\nmy class wrote poems using the idea:\nhttps://t.co/rkxYXgboNW\n@cogdog might recognise the featured image;-)",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			212
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tdc3254",
+					"indices": [
+						9,
+						17
+					]
+				},
+				{
+					"text": "ds106",
+					"indices": [
+						18,
+						24
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ds106dc",
+					"name": "ds106 Daily Create",
+					"id": 995562187,
+					"id_str": "995562187",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "cogdog",
+					"name": "Alan Levine",
+					"id": 740343,
+					"id_str": "740343",
+					"indices": [
+						167,
+						174
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/nFIp498rq1",
+					"expanded_url": "http://www-personal.umich.edu/~rwtill/Poems/table.html",
+					"display_url": "www-personal.umich.edu/~rwtill/Poems/…",
+					"indices": [
+						81,
+						104
+					]
+				},
+				{
+					"url": "https://t.co/rkxYXgboNW",
+					"expanded_url": "https://blogs.glowscotland.org.uk/nl/bantonbiggies/2020/09/11/poems-on-the-table/",
+					"display_url": "blogs.glowscotland.org.uk/nl/bantonbiggi…",
+					"indices": [
+						143,
+						166
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1336540586841542700,
+		"in_reply_to_status_id_str": "1336540586841542657",
+		"in_reply_to_user_id": 995562187,
+		"in_reply_to_user_id_str": "995562187",
+		"in_reply_to_screen_name": "ds106dc",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3931,
+			"friends_count": 2666,
+			"listed_count": 267,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5695,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18783,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ds106dc #tdc3254 #ds106 there is a lovely poem about putting things on a table:\nhttps://t.co/nFIp498rq1\n\nmy class wrote poems using the idea:\nhttps://t.co/rkxYXgboNW\n@cogdog might recognise the featured image;-)"
+	},
+	{
 		"created_at": "Tue Dec 08 19:59:41 +0000 2020",
 		"id": 1336400074595831800,
 		"id_str": "1336400074595831809",
