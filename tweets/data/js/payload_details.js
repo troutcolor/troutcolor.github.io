b229e5a93b358e78b964b3367b1cf80a17754a1b
John Johnston <johnjohnston@mac.com>
2021-01-10 00:17:48 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18630,
-	"created_at": "Sat Jan 09 00:17:47 +0000 2021",
+	"tweets": 18631,
+	"created_at": "Sun Jan 10 00:17:47 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_01.js
+++ tweets/data/js/tweets/2021_01.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sat Jan 09 05:27:28 +0000 2021",
+		"id": 1347776987377627100,
+		"id_str": "1347776987377627141",
+		"full_text": "@erinroseglass @ProjectReboot ... how this can be addresses in primary &amp; secondary education. The normalisation of the culture begins, surely,  in the home &amp; early education. I wonder if educators even start to consider this problem. In my own sector, primary, where would we start? https://t.co/HqYPQa5WLd",
+		"truncated": false,
+		"display_text_range": [
+			30,
+			314
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "erinroseglass",
+					"name": "erin glass",
+					"id": 468076687,
+					"id_str": "468076687",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "ProjectReboot",
+					"name": "The Reboot",
+					"id": 1308176969406623700,
+					"id_str": "1308176969406623744",
+					"indices": [
+						15,
+						29
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/HqYPQa5WLd",
+					"expanded_url": "https://johnjohnston.info/blog/?p=14050",
+					"display_url": "johnjohnston.info/blog/?p=14050",
+					"indices": [
+						291,
+						314
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1346875841662259200,
+		"in_reply_to_status_id_str": "1346875841662259202",
+		"in_reply_to_user_id": 468076687,
+		"in_reply_to_user_id_str": "468076687",
+		"in_reply_to_screen_name": "erinroseglass",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3929,
+			"friends_count": 2669,
+			"listed_count": 265,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5751,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18849,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@erinroseglass @ProjectReboot ... how this can be addresses in primary &amp; secondary education. The normalisation of the culture begins, surely,  in the home &amp; early education. I wonder if educators even start to consider this problem. In my own sector, primary, where would we start? https://t.co/HqYPQa5WLd"
+	},
+	{
 		"created_at": "Fri Jan 08 09:21:41 +0000 2021",
 		"id": 1347473539964809200,
 		"id_str": "1347473539964809219",
