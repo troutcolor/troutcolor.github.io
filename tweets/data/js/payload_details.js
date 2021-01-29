1b2d6891b75c75f8f71498260bb895137275e4fd
John Johnston <johnjohnston@mac.com>
2021-01-29 00:17:49 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18649,
-	"created_at": "Thu Jan 28 00:17:48 +0000 2021",
+	"tweets": 18650,
+	"created_at": "Fri Jan 29 00:17:48 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_01.js
+++ tweets/data/js/tweets/2021_01.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Thu Jan 28 10:16:27 +0000 2021",
+		"id": 1354735078337880000,
+		"id_str": "1354735078337880066",
+		"full_text": "An almost complete list of external content you can embed in  #GlowBlogs #GlowScot including a few new ones.\n\nhttps://t.co/o1O3UCorWL https://t.co/JkK6XBlEPW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			133
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GlowBlogs",
+					"indices": [
+						62,
+						72
+					]
+				},
+				{
+					"text": "GlowScot",
+					"indices": [
+						73,
+						82
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/o1O3UCorWL",
+					"expanded_url": "https://blogs.glowscotland.org.uk/glowblogs/embeds/",
+					"display_url": "blogs.glowscotland.org.uk/glowblogs/embe…",
+					"indices": [
+						110,
+						133
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1354733865135444000,
+					"id_str": "1354733865135443968",
+					"indices": [
+						134,
+						157
+					],
+					"media_url": "http://pbs.twimg.com/media/Esz7RcyXEAAuBNg.png",
+					"media_url_https": "https://pbs.twimg.com/media/Esz7RcyXEAAuBNg.png",
+					"url": "https://t.co/JkK6XBlEPW",
+					"display_url": "pic.twitter.com/JkK6XBlEPW",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1354735078337880066/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 886,
+							"h": 444,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 886,
+							"h": 444,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 341,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1354733865135444000,
+					"id_str": "1354733865135443968",
+					"indices": [
+						134,
+						157
+					],
+					"media_url": "http://pbs.twimg.com/media/Esz7RcyXEAAuBNg.png",
+					"media_url_https": "https://pbs.twimg.com/media/Esz7RcyXEAAuBNg.png",
+					"url": "https://t.co/JkK6XBlEPW",
+					"display_url": "pic.twitter.com/JkK6XBlEPW",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1354735078337880066/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 886,
+							"h": 444,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 886,
+							"h": 444,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 341,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3924,
+			"friends_count": 2670,
+			"listed_count": 265,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5779,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18876,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "An almost complete list of external content you can embed in  #GlowBlogs #GlowScot including a few new ones.\n\nhttps://t.co/o1O3UCorWL https://t.co/JkK6XBlEPW"
+	},
+	{
 		"created_at": "Wed Jan 27 20:27:15 +0000 2021",
 		"id": 1354526403530805200,
 		"id_str": "1354526403530805250",
