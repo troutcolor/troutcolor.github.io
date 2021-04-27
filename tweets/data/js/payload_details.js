2ee8ff1f3a452126557e749d9ed9a3a8cef2f51c
John Johnston <johnjohnston@mac.com>
2021-04-27 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18729,
-	"created_at": "Sun Apr 25 00:17:47 +0000 2021",
+	"tweets": 18730,
+	"created_at": "Tue Apr 27 00:17:47 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_04.js
+++ tweets/data/js/tweets/2021_04.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Mon Apr 26 19:01:46 +0000 2021",
+		"id": 1386560755709919200,
+		"id_str": "1386757411017564165",
+		"full_text": "Want to create a school website or improve the features on the one you have? Interested in finding out about how to share resources or set up e-Portfolios? Glow Blog Ideas showcases an array of uses for Glow Blogs &amp; all the help you need to get started!👇\nhttps://t.co/o8ykopIo29",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			282
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/o8ykopIo29",
+					"expanded_url": "http://ow.ly/TM2N50EvUjD",
+					"display_url": "ow.ly/TM2N50EvUjD",
+					"indices": [
+						259,
+						282
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2912429973,
+			"id_str": "2912429973",
+			"name": "Glow Scot",
+			"screen_name": "GlowScot",
+			"location": "Scotland",
+			"description": "Interested in Digital Learning and Teaching? So are we! This is the national Twitter account for Glow, Scotland's online environment for learning.",
+			"url": "https://t.co/JOnBEWBELo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/JOnBEWBELo",
+							"expanded_url": "https://glowconnect.org.uk/",
+							"display_url": "glowconnect.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19010,
+			"friends_count": 1396,
+			"listed_count": 117,
+			"created_at": "Tue Dec 09 09:39:02 +0000 2014",
+			"favourites_count": 7026,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6609,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/542261038574358528/PUswggVZ_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/542261038574358528/PUswggVZ_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2912429973/1521453812",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Want to create a school website or improve the features on the one you have? Interested in finding out about how to share resources or set up e-Portfolios? Glow Blog Ideas showcases an array of uses for Glow Blogs &amp; all the help you need to get started!👇\nhttps://t.co/o8ykopIo29"
+	},
+	{
 		"created_at": "Sat Apr 24 13:22:56 +0000 2021",
 		"id": 1385947368403521500,
 		"id_str": "1385947368403521536",
