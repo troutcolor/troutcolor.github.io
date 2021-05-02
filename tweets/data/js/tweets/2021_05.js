0497692d02852788b7c91c8907345534a3ccd0f8
John Johnston <johnjohnston@mac.com>
2021-05-02 01:17:48 +0100

    Added 1 tweets

---
+++ tweets/data/js/tweets/2021_05.js
@@ -0,0 +1,140 @@
+Grailbird.data.tweets_2021_05 = 
+[
+	{
+		"created_at": "Sat May 01 09:58:42 +0000 2021",
+		"id": 1388432685358538800,
+		"id_str": "1388432685358538753",
+		"full_text": "@FifeCYPIC @Burntisland_PS &gt; Provision of IT equipment to a family to overcome digital exclusion did not increase their engagement with online learning\n\nNot unexpected. But I am finding this series of posters from @FifeCYPIC really interesting. Wonder if they are available as pdfs?",
+		"truncated": false,
+		"display_text_range": [
+			27,
+			285
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "FifeCYPIC",
+					"name": "Fife CYPIC",
+					"id": 1375095252621668400,
+					"id_str": "1375095252621668356",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "Burntisland_PS",
+					"name": "Burntisland PS",
+					"id": 4913322059,
+					"id_str": "4913322059",
+					"indices": [
+						11,
+						26
+					]
+				},
+				{
+					"screen_name": "FifeCYPIC",
+					"name": "Fife CYPIC",
+					"id": 1375095252621668400,
+					"id_str": "1375095252621668356",
+					"indices": [
+						217,
+						227
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": 1382248615687639000,
+		"in_reply_to_status_id_str": "1382248615687639043",
+		"in_reply_to_user_id": 1375095252621668400,
+		"in_reply_to_user_id_str": "1375095252621668356",
+		"in_reply_to_screen_name": "FifeCYPIC",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3910,
+			"friends_count": 2674,
+			"listed_count": 265,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5828,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18987,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@FifeCYPIC @Burntisland_PS &gt; Provision of IT equipment to a family to overcome digital exclusion did not increase their engagement with online learning\n\nNot unexpected. But I am finding this series of posters from @FifeCYPIC really interesting. Wonder if they are available as pdfs?"
+	}
+]
