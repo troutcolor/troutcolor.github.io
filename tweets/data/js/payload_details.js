09d8c5a65a45742cd9f5759e54dfe42490a3fb7c
John Johnston <johnjohnston@mac.com>
2021-06-23 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18788,
-	"created_at": "Mon Jun 21 00:17:47 +0000 2021",
+	"tweets": 18789,
+	"created_at": "Wed Jun 23 00:17:47 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_06.js
+++ tweets/data/js/tweets/2021_06.js
@@ -1,6 +1,154 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Tue Jun 22 12:47:52 +0000 2021",
+		"id": 1407319425523622000,
+		"id_str": "1407319425523621892",
+		"full_text": "@olliebray @TinkeringStudio Thanks, Love @TinkeringStudio we borrowed one of their ideas last session: https://t.co/zj6DkDeTlS and had a marvellous time",
+		"truncated": false,
+		"display_text_range": [
+			28,
+			152
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "olliebray",
+					"name": "Ollie Bray",
+					"id": 6896272,
+					"id_str": "6896272",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "TinkeringStudio",
+					"name": "The Tinkering Studio",
+					"id": 288505724,
+					"id_str": "288505724",
+					"indices": [
+						11,
+						27
+					]
+				},
+				{
+					"screen_name": "TinkeringStudio",
+					"name": "The Tinkering Studio",
+					"id": 288505724,
+					"id_str": "288505724",
+					"indices": [
+						41,
+						57
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/zj6DkDeTlS",
+					"expanded_url": "https://blogs.glowscotland.org.uk/nl/bantonbiggies/tag/cardboard_automata/",
+					"display_url": "blogs.glowscotland.org.uk/nl/bantonbiggi…",
+					"indices": [
+						103,
+						126
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1407302814922162200,
+		"in_reply_to_status_id_str": "1407302814922162183",
+		"in_reply_to_user_id": 6896272,
+		"in_reply_to_user_id_str": "6896272",
+		"in_reply_to_screen_name": "olliebray",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3884,
+			"friends_count": 2674,
+			"listed_count": 265,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5867,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19055,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@olliebray @TinkeringStudio Thanks, Love @TinkeringStudio we borrowed one of their ideas last session: https://t.co/zj6DkDeTlS and had a marvellous time"
+	},
+	{
 		"created_at": "Sun Jun 20 18:37:46 +0000 2021",
 		"id": 1406682706801565700,
 		"id_str": "1406682706801565706",
