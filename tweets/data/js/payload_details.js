a7cda85b4183f8141259bb21cff4ca559adf8486
John Johnston <johnjohnston@mac.com>
2021-08-20 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18854,
-	"created_at": "Wed Aug 18 00:17:47 +0000 2021",
+	"tweets": 18855,
+	"created_at": "Fri Aug 20 00:17:47 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_08.js
+++ tweets/data/js/tweets/2021_08.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 19 20:40:51 +0000 2021",
+		"id": 1428440221813723100,
+		"id_str": "1428456950245830669",
+		"full_text": "People have been asking what @AvantisEd does. Therefore I am running an informal Webinar to look at \n@ClassVR and @avantisworld and #VRARinEDU \nThis is open to absolutely any educator any where in the world.\nThursday 26th 4pm BST 40 mins Bring a coffee\nhttps://t.co/6sEjAEGpi3 https://t.co/Xpbh8aZopM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "VRARinEDU",
+					"indices": [
+						132,
+						142
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AvantisEd",
+					"name": "Avantis Education",
+					"id": 4746560194,
+					"id_str": "4746560194",
+					"indices": [
+						29,
+						39
+					]
+				},
+				{
+					"screen_name": "ClassVR",
+					"name": "ClassVR from Avantis",
+					"id": 801006263308001300,
+					"id_str": "801006263308001280",
+					"indices": [
+						101,
+						109
+					]
+				},
+				{
+					"screen_name": "avantisworld",
+					"name": "Avanti's World",
+					"id": 1333791514850279400,
+					"id_str": "1333791514850279425",
+					"indices": [
+						114,
+						127
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/6sEjAEGpi3",
+					"expanded_url": "https://www.classvr.com/webinar-virtual-reality-in-education/",
+					"display_url": "classvr.com/webinar-virtua…",
+					"indices": [
+						253,
+						276
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1428440159469584400,
+					"id_str": "1428440159469584390",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/E9LWwcAWQAYcZcT.png",
+					"media_url_https": "https://pbs.twimg.com/media/E9LWwcAWQAYcZcT.png",
+					"url": "https://t.co/Xpbh8aZopM",
+					"display_url": "pic.twitter.com/Xpbh8aZopM",
+					"expanded_url": "https://twitter.com/IanStuart66/status/1428440221813723137/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 900,
+							"h": 404,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 404,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 305,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1428440159469584400,
+					"id_str": "1428440159469584390",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/E9LWwcAWQAYcZcT.png",
+					"media_url_https": "https://pbs.twimg.com/media/E9LWwcAWQAYcZcT.png",
+					"url": "https://t.co/Xpbh8aZopM",
+					"display_url": "pic.twitter.com/Xpbh8aZopM",
+					"expanded_url": "https://twitter.com/IanStuart66/status/1428440221813723137/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 900,
+							"h": 404,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 404,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 305,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2228692152,
+			"id_str": "2228692152",
+			"name": "Ian Stuart",
+			"screen_name": "IanStuart66",
+			"location": "Edinburgh, Scotland",
+			"description": "Formerly an Engineer, then an Educator, Consultant, Microsoft Edu. Now Head of Education with @AvantisEd\nhttps://t.co/CaYQ4Fk0J8",
+			"url": "https://t.co/7WS9fy1yLF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7WS9fy1yLF",
+							"expanded_url": "https://www.linkedin.com/in/istuart1/",
+							"display_url": "linkedin.com/in/istuart1/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/CaYQ4Fk0J8",
+							"expanded_url": "https://www.avantiseducation.com/",
+							"display_url": "avantiseducation.com",
+							"indices": [
+								105,
+								128
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 6768,
+			"friends_count": 6689,
+			"listed_count": 429,
+			"created_at": "Tue Dec 03 18:38:41 +0000 2013",
+			"favourites_count": 5844,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 53368,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/777067782852710400/0H-hfeZU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/777067782852710400/0H-hfeZU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2228692152/1386114209",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "People have been asking what @AvantisEd does. Therefore I am running an informal Webinar to look at \n@ClassVR and @avantisworld and #VRARinEDU \nThis is open to absolutely any educator any where in the world.\nThursday 26th 4pm BST 40 mins Bring a coffee\nhttps://t.co/6sEjAEGpi3 https://t.co/Xpbh8aZopM"
+	},
+	{
 		"created_at": "Tue Aug 17 04:17:26 +0000 2021",
 		"id": 1427484692513796000,
 		"id_str": "1427484692513796098",
