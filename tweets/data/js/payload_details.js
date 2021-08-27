0dac6026aacbfc56b525038132dec119a2c485c0
John Johnston <johnjohnston@mac.com>
2021-08-27 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18862,
-	"created_at": "Mon Aug 23 00:17:47 +0000 2021",
+	"tweets": 18863,
+	"created_at": "Fri Aug 27 00:17:47 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_08.js
+++ tweets/data/js/tweets/2021_08.js
@@ -1,6 +1,144 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 26 14:15:19 +0000 2021",
+		"id": 1430896644862611500,
+		"id_str": "1430896644862611462",
+		"full_text": "@rooferrocks @GlowScot Sorry about that. Unexpected consequence of the latest release. Please ignore the messages for now. We hope to stop them.  More info about the release: https://t.co/jnQ0wEAAXp",
+		"truncated": false,
+		"display_text_range": [
+			23,
+			198
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "rooferrocks",
+					"name": "Ruth Burnett",
+					"id": 49728490,
+					"id_str": "49728490",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "GlowScot",
+					"name": "Glow Scot",
+					"id": 2912429973,
+					"id_str": "2912429973",
+					"indices": [
+						13,
+						22
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/jnQ0wEAAXp",
+					"expanded_url": "https://blogs.glowscotland.org.uk/glowblogs/glowbloghelp/2021/08/26/glow-blogs-update-26-aug-2021/",
+					"display_url": "blogs.glowscotland.org.uk/glowblogs/glow…",
+					"indices": [
+						175,
+						198
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1430590222098747400,
+		"in_reply_to_status_id_str": "1430590222098747394",
+		"in_reply_to_user_id": 49728490,
+		"in_reply_to_user_id_str": "49728490",
+		"in_reply_to_screen_name": "rooferrocks",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3883,
+			"friends_count": 2681,
+			"listed_count": 262,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5957,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19163,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@rooferrocks @GlowScot Sorry about that. Unexpected consequence of the latest release. Please ignore the messages for now. We hope to stop them.  More info about the release: https://t.co/jnQ0wEAAXp"
+	},
+	{
 		"created_at": "Sun Aug 22 21:39:33 +0000 2021",
 		"id": 1429558888676929500,
 		"id_str": "1429558888676929542",
