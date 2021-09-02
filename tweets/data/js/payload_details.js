68114486d65b5ac971c20002df71cea12b5c8b55
John Johnston <johnjohnston@mac.com>
2021-09-02 01:17:49 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18868,
-	"created_at": "Wed Sep 01 00:17:47 +0000 2021",
+	"tweets": 18869,
+	"created_at": "Thu Sep 02 00:17:47 +0000 2021",
 	"lang": "en"
 }
---
+++ tweets/data/js/tweets/2021_09.js
@@ -0,0 +1,211 @@
+Grailbird.data.tweets_2021_09 = 
+[
+	{
+		"created_at": "Wed Sep 01 09:43:24 +0000 2021",
+		"id": 1432993448693538800,
+		"id_str": "1433002540224892928",
+		"full_text": "Today starts #WorldAlzheimersMonth Personally meaningful music can highlight the 'me' in #dementia &amp; help the person shine through 💚🎵\n\nDiscover how over at @PlaylistforLife &amp; help spread the word #SongsforDementia https://t.co/SIg9ODeb3l",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			221
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "WorldAlzheimersMonth",
+					"indices": [
+						13,
+						34
+					]
+				},
+				{
+					"text": "dementia",
+					"indices": [
+						89,
+						98
+					]
+				},
+				{
+					"text": "SongsforDementia",
+					"indices": [
+						204,
+						221
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PlaylistforLife",
+					"name": "Playlist for Life",
+					"id": 2203221589,
+					"id_str": "2203221589",
+					"indices": [
+						160,
+						176
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1432993445946269700,
+					"id_str": "1432993445946269698",
+					"indices": [
+						222,
+						245
+					],
+					"media_url": "http://pbs.twimg.com/media/E-MD8l8XIAI8UVA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-MD8l8XIAI8UVA.jpg",
+					"url": "https://t.co/SIg9ODeb3l",
+					"display_url": "pic.twitter.com/SIg9ODeb3l",
+					"expanded_url": "https://twitter.com/MTImmonsMusic/status/1432993448693538818/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1432993445946269700,
+					"id_str": "1432993445946269698",
+					"indices": [
+						222,
+						245
+					],
+					"media_url": "http://pbs.twimg.com/media/E-MD8l8XIAI8UVA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-MD8l8XIAI8UVA.jpg",
+					"url": "https://t.co/SIg9ODeb3l",
+					"display_url": "pic.twitter.com/SIg9ODeb3l",
+					"expanded_url": "https://twitter.com/MTImmonsMusic/status/1432993448693538818/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1244438078,
+			"id_str": "1244438078",
+			"name": "Michael Timmons",
+			"screen_name": "MTImmonsMusic",
+			"location": "",
+			"description": "Solo musician with sad songs from Scotland + Head of Communities at @playlistforlife .\nViews my own",
+			"url": "https://t.co/rKw1LCn9B0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/rKw1LCn9B0",
+							"expanded_url": "http://michaeltimmonsmusic.bandcamp.com",
+							"display_url": "michaeltimmonsmusic.bandcamp.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 790,
+			"friends_count": 512,
+			"listed_count": 13,
+			"created_at": "Tue Mar 05 19:16:35 +0000 2013",
+			"favourites_count": 6923,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3751,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/848934481096474624/qKZCEFdR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/848934481096474624/qKZCEFdR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1244438078/1562073557",
+			"profile_link_color": "141414",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today starts #WorldAlzheimersMonth Personally meaningful music can highlight the 'me' in #dementia &amp; help the person shine through 💚🎵\n\nDiscover how over at @PlaylistforLife &amp; help spread the word #SongsforDementia https://t.co/SIg9ODeb3l"
+	}
+]
