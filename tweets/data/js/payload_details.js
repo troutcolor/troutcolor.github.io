2fc99dd26a385618a879e1f988909037ea6e26a7
John Johnston <johnjohnston@mac.com>
2021-10-10 01:17:48 +0100

    Added 2 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18904,
-	"created_at": "Sat Oct 09 00:17:47 +0000 2021",
+	"tweets": 18906,
+	"created_at": "Sun Oct 10 00:17:47 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_10.js
+++ tweets/data/js/tweets/2021_10.js
@@ -1,6 +1,208 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sat Oct 09 21:02:13 +0000 2021",
+		"id": 1446792736926019600,
+		"id_str": "1446944109378342919",
+		"full_text": "Google is apparently preparing to re-enter the market for RSS readers. \n\nBefore they do, shouldn’t they explain why they dumped RSS, hurting the cause enormously, and why anyone should trust them this time? \n\nPlease RT so users understand, they are not our friends.\n\nThanks.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3839,
+			"id_str": "3839",
+			"name": "Dave Winer",
+			"screen_name": "davewiner",
+			"location": "Woodstock, NY",
+			"description": "I create new media types for fun. Developer of outliners, blogging tools, RSS aggregators, podcasting.",
+			"url": "https://t.co/ztgzDGiyOj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ztgzDGiyOj",
+							"expanded_url": "http://scripting.com/",
+							"display_url": "scripting.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 66705,
+			"friends_count": 3903,
+			"listed_count": 5256,
+			"created_at": "Sat Aug 05 23:04:08 +0000 2006",
+			"favourites_count": 18190,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 196345,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398806004528263168/2SXipvrt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398806004528263168/2SXipvrt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3839/1632793593",
+			"profile_link_color": "7F7F90",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E1E1DF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 125,
+		"favorite_count": 189,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Google is apparently preparing to re-enter the market for RSS readers. \n\nBefore they do, shouldn’t they explain why they dumped RSS, hurting the cause enormously, and why anyone should trust them this time? \n\nPlease RT so users understand, they are not our friends.\n\nThanks."
+	},
+	{
+		"created_at": "Sat Oct 09 19:29:57 +0000 2021",
+		"id": 1446920892307673000,
+		"id_str": "1446920892307673088",
+		"full_text": "Watched: HARAKIRI (1962 Film) : SHOCHIKU\nGreat movie. Full of quiet and stillness. According to Wikipedia it lost the Palme d’Or to the Leopard, another film I love. 🎥",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			167
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3884,
+			"friends_count": 2678,
+			"listed_count": 0,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 5991,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19219,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "Watched: HARAKIRI (1962 Film) : SHOCHIKU\nGreat movie. Full of quiet and stillness. According to Wikipedia it lost the Palme d’Or to the Leopard, another film I love. 🎥"
+	},
+	{
 		"created_at": "Fri Oct 08 21:11:37 +0000 2021",
 		"id": 1446584088874438700,
 		"id_str": "1446584088874438662",
