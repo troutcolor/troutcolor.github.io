5bd546b49f5ae37f5d8fb666b0176ed8f990b865
John Johnston <johnjohnston@mac.com>
2021-11-05 00:17:49 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18926,
-	"created_at": "Thu Nov 04 00:17:48 +0000 2021",
+	"tweets": 18927,
+	"created_at": "Fri Nov 05 00:17:48 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_11.js
+++ tweets/data/js/tweets/2021_11.js
@@ -1,6 +1,154 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Thu Nov 04 17:33:20 +0000 2021",
+		"id": 1456313627900792800,
+		"id_str": "1456313627900792846",
+		"full_text": "@LouiseE_Foreman @CodingLorna @codeclubscot Very nice event. My kids enjoyed themselves. Might be worth mentioning that you can paste a url to a published scratch file into GlowBlogs and it embeds. \n\nhttps://t.co/dgKntXhbUB",
+		"truncated": false,
+		"display_text_range": [
+			44,
+			223
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LouiseE_Foreman",
+					"name": "Louise Foreman",
+					"id": 819287928,
+					"id_str": "819287928",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "CodingLorna",
+					"name": "Lorna Gibson",
+					"id": 2734167120,
+					"id_str": "2734167120",
+					"indices": [
+						17,
+						29
+					]
+				},
+				{
+					"screen_name": "codeclubscot",
+					"name": "Code Club Scotland",
+					"id": 3236641407,
+					"id_str": "3236641407",
+					"indices": [
+						30,
+						43
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/dgKntXhbUB",
+					"expanded_url": "https://blogs.glowscotland.org.uk/glowblogs/embeds/2020/12/12/scratch/",
+					"display_url": "blogs.glowscotland.org.uk/glowblogs/embe…",
+					"indices": [
+						200,
+						223
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1456254369922388000,
+		"in_reply_to_status_id_str": "1456254369922387974",
+		"in_reply_to_user_id": 819287928,
+		"in_reply_to_user_id_str": "819287928",
+		"in_reply_to_screen_name": "LouiseE_Foreman",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3891,
+			"friends_count": 2683,
+			"listed_count": 259,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 6006,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19252,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@LouiseE_Foreman @CodingLorna @codeclubscot Very nice event. My kids enjoyed themselves. Might be worth mentioning that you can paste a url to a published scratch file into GlowBlogs and it embeds. \n\nhttps://t.co/dgKntXhbUB"
+	},
+	{
 		"created_at": "Wed Nov 03 22:03:13 +0000 2021",
 		"id": 1456019159624597500,
 		"id_str": "1456019159624597508",
