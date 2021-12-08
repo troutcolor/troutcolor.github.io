1fb7c9bade4b1b2dcc64e56a13ff8b8e938198df
John Johnston <johnjohnston@mac.com>
2021-12-08 00:17:48 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18944,
-	"created_at": "Tue Dec 07 00:17:47 +0000 2021",
+	"tweets": 18945,
+	"created_at": "Wed Dec 08 00:17:47 +0000 2021",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2021_12.js
+++ tweets/data/js/tweets/2021_12.js
@@ -1,6 +1,453 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Tue Dec 07 20:13:24 +0000 2021",
+		"id": 1468312710702112800,
+		"id_str": "1468312710702112769",
+		"full_text": "@Banton_Pr Biggies created pages for this book. https://t.co/Q3MPtbev1T",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			47
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Banton_Pr",
+					"name": "Banton Primary",
+					"id": 702565959354163200,
+					"id_str": "702565959354163200",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Q3MPtbev1T",
+					"expanded_url": "https://twitter.com/mckean_mrr/status/1468300824283594754",
+					"display_url": "twitter.com/mckean_mrr/sta…",
+					"indices": [
+						48,
+						71
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 702565959354163200,
+		"in_reply_to_user_id_str": "702565959354163200",
+		"in_reply_to_screen_name": "Banton_Pr",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3890,
+			"friends_count": 2684,
+			"listed_count": 0,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 6039,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19274,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "791e00bcadc4615f",
+			"url": "https://api.twitter.com/1.1/geo/id/791e00bcadc4615f.json",
+			"place_type": "city",
+			"name": "Glasgow",
+			"full_name": "Glasgow, Scotland",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-4.3932845,
+							55.796184
+						],
+						[
+							-4.0902182,
+							55.796184
+						],
+						[
+							-4.0902182,
+							55.9204214
+						],
+						[
+							-4.3932845,
+							55.9204214
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1468300824283594800,
+		"quoted_status_id_str": "1468300824283594754",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Q3MPtbev1T",
+			"expanded": "https://twitter.com/mckean_mrr/status/1468300824283594754",
+			"display": "twitter.com/mckean_mrr/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Dec 07 19:26:10 +0000 2021",
+			"id": 1468300824283594800,
+			"id_str": "1468300824283594754",
+			"full_text": "@GlasgowRTC @DL_for_All @KingsparkP @EmmaCackette @StPhilomenasRC @lamont_ms @Mrs_Wilson_22 @ictcrowd @johnjohnston @MountFloridaPri @kelly_cflc @CranhillP @Holyrood_Art @haire_ms @allsaintsrcsec you’re in the latest @BookCreatorApp newsletter! @EdISGlasgow #OurDearGreenPlace. https://t.co/HTyTdbgP7N",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				277
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "OurDearGreenPlace",
+						"indices": [
+							258,
+							276
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "GlasgowRTC",
+						"name": "GlasgowRTC",
+						"id": 1180824648176537600,
+						"id_str": "1180824648176537600",
+						"indices": [
+							0,
+							11
+						]
+					},
+					{
+						"screen_name": "DL_for_All",
+						"name": "Digital Learning for ALL",
+						"id": 970802301857423400,
+						"id_str": "970802301857423360",
+						"indices": [
+							12,
+							23
+						]
+					},
+					{
+						"screen_name": "KingsparkP",
+						"name": "King's Park Primary",
+						"id": 2373595649,
+						"id_str": "2373595649",
+						"indices": [
+							24,
+							35
+						]
+					},
+					{
+						"screen_name": "EmmaCackette",
+						"name": "Emma Cackette",
+						"id": 1037032252813795300,
+						"id_str": "1037032252813795330",
+						"indices": [
+							36,
+							49
+						]
+					},
+					{
+						"screen_name": "StPhilomenasRC",
+						"name": "St Philomena's RC",
+						"id": 766583731289194500,
+						"id_str": "766583731289194496",
+						"indices": [
+							50,
+							65
+						]
+					},
+					{
+						"screen_name": "lamont_ms",
+						"name": "Ms Lamont",
+						"id": 1006634194255130600,
+						"id_str": "1006634194255130624",
+						"indices": [
+							66,
+							76
+						]
+					},
+					{
+						"screen_name": "Mrs_Wilson_22",
+						"name": "Mrs Wilson",
+						"id": 1159151349172494300,
+						"id_str": "1159151349172494337",
+						"indices": [
+							77,
+							91
+						]
+					},
+					{
+						"screen_name": "ictcrowd",
+						"name": "George Milliken",
+						"id": 824382054657835000,
+						"id_str": "824382054657835012",
+						"indices": [
+							92,
+							101
+						]
+					},
+					{
+						"screen_name": "johnjohnston",
+						"name": "john johnston",
+						"id": 7484192,
+						"id_str": "7484192",
+						"indices": [
+							102,
+							115
+						]
+					},
+					{
+						"screen_name": "MountFloridaPri",
+						"name": "Mount Florida Primary and Nursery Class",
+						"id": 955205584768430100,
+						"id_str": "955205584768430080",
+						"indices": [
+							116,
+							132
+						]
+					},
+					{
+						"screen_name": "kelly_cflc",
+						"name": "KellySloan",
+						"id": 1049635438745571300,
+						"id_str": "1049635438745571331",
+						"indices": [
+							133,
+							144
+						]
+					},
+					{
+						"screen_name": "CranhillP",
+						"name": "CranhillPrimary",
+						"id": 1090348839767298000,
+						"id_str": "1090348839767298048",
+						"indices": [
+							145,
+							155
+						]
+					},
+					{
+						"screen_name": "Holyrood_Art",
+						"name": "Holyrood Secondary Art & Design",
+						"id": 975107213877489700,
+						"id_str": "975107213877489666",
+						"indices": [
+							156,
+							169
+						]
+					},
+					{
+						"screen_name": "haire_ms",
+						"name": "Carol Haire",
+						"id": 914169731875262500,
+						"id_str": "914169731875262465",
+						"indices": [
+							170,
+							179
+						]
+					},
+					{
+						"screen_name": "allsaintsrcsec",
+						"name": "All Saints' RC Secondary",
+						"id": 2976476602,
+						"id_str": "2976476602",
+						"indices": [
+							180,
+							195
+						]
+					},
+					{
+						"screen_name": "BookCreatorApp",
+						"name": "Book Creator Team",
+						"id": 236830345,
+						"id_str": "236830345",
+						"indices": [
+							217,
+							232
+						]
+					},
+					{
+						"screen_name": "EdISGlasgow",
+						"name": "EdIS Glasgow",
+						"id": 1083835379751534600,
+						"id_str": "1083835379751534592",
+						"indices": [
+							245,
+							257
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/HTyTdbgP7N",
+						"expanded_url": "https://twitter.com/BookCreatorApp/status/1466033693319958535",
+						"display_url": "twitter.com/BookCreatorApp…",
+						"indices": [
+							278,
+							301
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": 1180824648176537600,
+			"in_reply_to_user_id_str": "1180824648176537600",
+			"in_reply_to_screen_name": "GlasgowRTC",
+			"user": {
+				"id": 840610620223430700,
+				"id_str": "840610620223430656",
+				"name": "Mr McKean",
+				"screen_name": "mckean_mrr",
+				"location": "Glasgow, Scotland",
+				"description": "DHT @KingsparkP & @GlasgowRTC manager | #AppleTeacher+Swift + #MIEExpert | ComputationalThinking | RT not endorsements",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 358,
+				"friends_count": 398,
+				"listed_count": 3,
+				"created_at": "Sat Mar 11 17:09:24 +0000 2017",
+				"favourites_count": 1339,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 974,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1356901515940802560/ha4wtml7_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1356901515940802560/ha4wtml7_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/840610620223430656/1567494930",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1466033693319958500,
+			"quoted_status_id_str": "1466033693319958535",
+			"quoted_status_permalink": {
+				"url": "https://t.co/HTyTdbgP7N",
+				"expanded": "https://twitter.com/BookCreatorApp/status/1466033693319958535",
+				"display": "twitter.com/BookCreatorApp…"
+			},
+			"retweet_count": 2,
+			"favorite_count": 11,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": true,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@Banton_Pr Biggies created pages for this book. https://t.co/Q3MPtbev1T"
+	},
+	{
 		"created_at": "Mon Dec 06 17:07:44 +0000 2021",
 		"id": 1467903595756638200,
 		"id_str": "1467903595756638211",
