905b0f6e991f5a42593a91aab129db17d389112e
John Johnston <johnjohnston@mac.com>
2022-05-06 01:17:47 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19076,
-	"created_at": "Thu May 05 00:17:54 +0000 2022",
+	"tweets": 19077,
+	"created_at": "Fri May 06 00:17:47 +0000 2022",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2022_05.js
+++ tweets/data/js/tweets/2022_05.js
@@ -1,6 +1,415 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Thu May 05 07:46:07 +0000 2022",
+		"id": 1522120442412249000,
+		"id_str": "1522120442412249088",
+		"full_text": "The WP Accessibility plugin in #GlowBlogs provided a nice reminder to help visitors. It lets you know if you forget to add Alt text. #Accessibility https://t.co/thM6VHuM9O https://t.co/mQffE6AqNu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			171
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GlowBlogs",
+					"indices": [
+						31,
+						41
+					]
+				},
+				{
+					"text": "Accessibility",
+					"indices": [
+						133,
+						147
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/thM6VHuM9O",
+					"expanded_url": "https://twitter.com/GlowScot/status/1522108945459990529",
+					"display_url": "twitter.com/GlowScot/statu…",
+					"indices": [
+						148,
+						171
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1522118959117578200,
+					"id_str": "1522118959117578241",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/FR-nIPgXMAEqXol.png",
+					"media_url_https": "https://pbs.twimg.com/media/FR-nIPgXMAEqXol.png",
+					"url": "https://t.co/mQffE6AqNu",
+					"display_url": "pic.twitter.com/mQffE6AqNu",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1522120442412249088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 697,
+							"h": 651,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 635,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 697,
+							"h": 651,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1522118959117578200,
+					"id_str": "1522118959117578241",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/FR-nIPgXMAEqXol.png",
+					"media_url_https": "https://pbs.twimg.com/media/FR-nIPgXMAEqXol.png",
+					"url": "https://t.co/mQffE6AqNu",
+					"display_url": "pic.twitter.com/mQffE6AqNu",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1522120442412249088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 697,
+							"h": 651,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 635,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 697,
+							"h": 651,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3880,
+			"friends_count": 2688,
+			"listed_count": 260,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 6177,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19446,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1522108945459990500,
+		"quoted_status_id_str": "1522108945459990529",
+		"quoted_status_permalink": {
+			"url": "https://t.co/thM6VHuM9O",
+			"expanded": "https://twitter.com/GlowScot/status/1522108945459990529",
+			"display": "twitter.com/GlowScot/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Thu May 05 07:00:26 +0000 2022",
+			"id": 1522108945459990500,
+			"id_str": "1522108945459990529",
+			"full_text": "⭐Quick wins with Glow⭐ When creating Glow Blogs that others will visit, accessibility should always be an important consideration. The WP Accessibility plugin supports this and is compatible with different themes! Try it out. https://t.co/31Bku5zr1c #LDWeek2022 https://t.co/OruHvgVIOC",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				261
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "LDWeek2022",
+						"indices": [
+							250,
+							261
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/31Bku5zr1c",
+						"expanded_url": "http://ow.ly/msGN50IUlgf",
+						"display_url": "ow.ly/msGN50IUlgf",
+						"indices": [
+							226,
+							249
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1522108944105226200,
+						"id_str": "1522108944105226240",
+						"indices": [
+							262,
+							285
+						],
+						"media_url": "http://pbs.twimg.com/media/FR-eBSrWQAAKwEl.png",
+						"media_url_https": "https://pbs.twimg.com/media/FR-eBSrWQAAKwEl.png",
+						"url": "https://t.co/OruHvgVIOC",
+						"display_url": "pic.twitter.com/OruHvgVIOC",
+						"expanded_url": "https://twitter.com/GlowScot/status/1522108945459990529/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 800,
+								"h": 800,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 800,
+								"h": 800,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1522108944105226200,
+						"id_str": "1522108944105226240",
+						"indices": [
+							262,
+							285
+						],
+						"media_url": "http://pbs.twimg.com/media/FR-eBSrWQAAKwEl.png",
+						"media_url_https": "https://pbs.twimg.com/media/FR-eBSrWQAAKwEl.png",
+						"url": "https://t.co/OruHvgVIOC",
+						"display_url": "pic.twitter.com/OruHvgVIOC",
+						"expanded_url": "https://twitter.com/GlowScot/status/1522108945459990529/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 800,
+								"h": 800,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 800,
+								"h": 800,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2912429973,
+				"id_str": "2912429973",
+				"name": "Glow Scot",
+				"screen_name": "GlowScot",
+				"location": "Scotland",
+				"description": "Interested in Digital Learning and Teaching? So are we! This is the national Twitter account for Glow, Scotland's online environment for learning.",
+				"url": "https://t.co/JOnBEWBELo",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/JOnBEWBELo",
+								"expanded_url": "https://glowconnect.org.uk/",
+								"display_url": "glowconnect.org.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 20302,
+				"friends_count": 1393,
+				"listed_count": 124,
+				"created_at": "Tue Dec 09 09:39:02 +0000 2014",
+				"favourites_count": 7497,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 7158,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/542261038574358528/PUswggVZ_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/542261038574358528/PUswggVZ_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2912429973/1521453812",
+				"profile_link_color": "9266CC",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": true,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The WP Accessibility plugin in #GlowBlogs provided a nice reminder to help visitors. It lets you know if you forget to add Alt text. #Accessibility https://t.co/thM6VHuM9O https://t.co/mQffE6AqNu"
+	},
+	{
 		"created_at": "Wed May 04 19:59:51 +0000 2022",
 		"id": 1521942702669152300,
 		"id_str": "1521942702669152258",
