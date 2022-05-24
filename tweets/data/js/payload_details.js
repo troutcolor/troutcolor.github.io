67c04e555c6d8847bcc36420bbd45ec6f5ba48ed
John Johnston <johnjohnston@mac.com>
2022-05-24 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19089,
-	"created_at": "Mon May 23 00:17:47 +0000 2022",
+	"tweets": 19090,
+	"created_at": "Tue May 24 00:17:47 +0000 2022",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2022_05.js
+++ tweets/data/js/tweets/2022_05.js
@@ -1,6 +1,134 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Mon May 23 19:42:17 +0000 2022",
+		"id": 1528819794711027700,
+		"id_str": "1528823653210677249",
+		"full_text": "@johnjohnston Delighted to hear it's now available in Glow. The best thing about it is that it's an open standard with easy import and export options making any interactive content easy to reuse. https://t.co/Bro2nWMyy4 has plenty of example activities to showcase the different activity types.",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			294
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "johnjohnston",
+					"name": "john johnston",
+					"id": 7484192,
+					"id_str": "7484192",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Bro2nWMyy4",
+					"expanded_url": "http://H5p.org",
+					"display_url": "H5p.org",
+					"indices": [
+						196,
+						219
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1528712137903902700,
+		"in_reply_to_status_id_str": "1528712137903902721",
+		"in_reply_to_user_id": 7484192,
+		"in_reply_to_user_id_str": "7484192",
+		"in_reply_to_screen_name": "johnjohnston",
+		"user": {
+			"id": 3126006784,
+			"id_str": "3126006784",
+			"name": "Peter Donaldson",
+			"screen_name": "MattersOfCS",
+			"location": "Scotland, United Kingdom",
+			"description": "University Lecturer of Computing Science Education @UofGEducation and @ccse_glasgow, Primary Technologies lead & webmaster of https://t.co/6n7p2McDtt",
+			"url": "https://t.co/reDEGem5vM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/reDEGem5vM",
+							"expanded_url": "https://gla.ac.uk/schools/education/staff/peterdonaldson/",
+							"display_url": "gla.ac.uk/schools/educat…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/6n7p2McDtt",
+							"expanded_url": "http://digitalliteracyframework.scot",
+							"display_url": "digitalliteracyframework.scot",
+							"indices": [
+								126,
+								149
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1774,
+			"friends_count": 2134,
+			"listed_count": 29,
+			"created_at": "Sun Mar 29 08:15:58 +0000 2015",
+			"favourites_count": 2084,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1816,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1286312980406206464/spHOQiva_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1286312980406206464/spHOQiva_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3126006784/1595515938",
+			"profile_link_color": "39AC73",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@johnjohnston Delighted to hear it's now available in Glow. The best thing about it is that it's an open standard with easy import and export options making any interactive content easy to reuse. https://t.co/Bro2nWMyy4 has plenty of example activities to showcase the different activity types."
+	},
+	{
 		"created_at": "Sun May 22 18:43:49 +0000 2022",
 		"id": 1528446552582283300,
 		"id_str": "1528446552582283272",
