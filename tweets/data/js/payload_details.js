3ca7dc04e1234d31bfa336198a2b812683a6cd5d
John Johnston <johnjohnston@mac.com>
2022-06-04 01:17:47 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19100,
-	"created_at": "Fri Jun 03 00:17:47 +0000 2022",
+	"tweets": 19101,
+	"created_at": "Sat Jun 04 00:17:47 +0000 2022",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2022_06.js
+++ tweets/data/js/tweets/2022_06.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Fri Jun 03 16:55:39 +0000 2022",
+		"id": 1532767984346927000,
+		"id_str": "1532767984346927106",
+		"full_text": "A walk around Ardinning this afternoon, bright, sunny and a bit of a breeze. Saw Common blue damselflies (as far as I can figure), and a couple of distant dragonflies. First for me this year. https://t.co/wW5xZmnD2W https://t.co/3Q9m92f7sD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			215
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/wW5xZmnD2W",
+					"expanded_url": "https://british-dragonflies.org.uk/species/common-blue-damselfly/",
+					"display_url": "british-dragonflies.org.uk/species/common…",
+					"indices": [
+						192,
+						215
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1532767982442750000,
+					"id_str": "1532767982442749953",
+					"indices": [
+						216,
+						239
+					],
+					"media_url": "http://pbs.twimg.com/media/FUV8W-yXoAEZfZ1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUV8W-yXoAEZfZ1.jpg",
+					"url": "https://t.co/3Q9m92f7sD",
+					"display_url": "pic.twitter.com/3Q9m92f7sD",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1532767984346927106/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 769,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 769,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1532767982442750000,
+					"id_str": "1532767982442749953",
+					"indices": [
+						216,
+						239
+					],
+					"media_url": "http://pbs.twimg.com/media/FUV8W-yXoAEZfZ1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUV8W-yXoAEZfZ1.jpg",
+					"url": "https://t.co/3Q9m92f7sD",
+					"display_url": "pic.twitter.com/3Q9m92f7sD",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1532767984346927106/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 769,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 769,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://micro.blog/\" rel=\"nofollow\">Micro.blog</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3889,
+			"friends_count": 2693,
+			"listed_count": 259,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 6200,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19479,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A walk around Ardinning this afternoon, bright, sunny and a bit of a breeze. Saw Common blue damselflies (as far as I can figure), and a couple of distant dragonflies. First for me this year. https://t.co/wW5xZmnD2W https://t.co/3Q9m92f7sD"
+	},
+	{
 		"created_at": "Thu Jun 02 20:13:56 +0000 2022",
 		"id": 1532455495293976600,
 		"id_str": "1532455495293976577",
