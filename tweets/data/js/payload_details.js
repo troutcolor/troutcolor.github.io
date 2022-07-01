994fb0e7788e6949409c1d3e5ccfb54938b8587b
John Johnston <johnjohnston@mac.com>
2022-07-01 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19116,
-	"created_at": "Thu Jun 30 00:17:48 +0000 2022",
+	"tweets": 19117,
+	"created_at": "Fri Jul 01 00:17:47 +0000 2022",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2022_06.js
+++ tweets/data/js/tweets/2022_06.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 30 10:20:00 +0000 2022",
+		"id": 1542397070836990000,
+		"id_str": "1542452888676581378",
+		"full_text": "UK government to scrap European law protecting special habitats - the Tories are destroying all that's good 😢 https://t.co/zO7GjRUcPS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			133
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zO7GjRUcPS",
+					"expanded_url": "https://www.theguardian.com/environment/2022/jun/30/uk-government-scrap-european-law-protecting-special-habitats?CMP=share_btn_tw",
+					"display_url": "theguardian.com/environment/20…",
+					"indices": [
+						110,
+						133
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3096453052,
+			"id_str": "3096453052",
+			"name": "Lesley Gunn",
+			"screen_name": "gunnlesley",
+			"location": "The Netherlands",
+			"description": "Woke Scot, at home in NL. Part-time translator, full-time animal lover 🐾 Guitar novice 🎸 Pro science, passionate about nature 🌱",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1897,
+			"friends_count": 2080,
+			"listed_count": 1,
+			"created_at": "Thu Mar 19 09:57:42 +0000 2015",
+			"favourites_count": 62578,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 19560,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1496833174076338178/bTpbjruJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1496833174076338178/bTpbjruJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3096453052/1645786988",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 981,
+		"favorite_count": 1226,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "UK government to scrap European law protecting special habitats - the Tories are destroying all that's good 😢 https://t.co/zO7GjRUcPS"
+	},
+	{
 		"created_at": "Wed Jun 29 19:59:42 +0000 2022",
 		"id": 1542236385855381500,
 		"id_str": "1542236385855381504",
