e615336eb6240002466107faa4f7c330d948101b
John Johnston <johnjohnston@mac.com>
2022-07-11 01:17:47 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19129,
-	"created_at": "Sat Jul 09 00:17:47 +0000 2022",
+	"tweets": 19130,
+	"created_at": "Mon Jul 11 00:17:47 +0000 2022",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2022_07.js
+++ tweets/data/js/tweets/2022_07.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Sun Jul 10 17:20:16 +0000 2022",
+		"id": 1546182529585225700,
+		"id_str": "1546182529585225735",
+		"full_text": "Back to Old Kilpatrick this morning to walk to Duncolm. Notes, photos &amp; map: walkmap https://t.co/3LhP0hQnmZ https://t.co/srh9hebhv1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			112
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3LhP0hQnmZ",
+					"expanded_url": "http://johnjohnston.info/walks/maps.html?f=72177720300442444&m=2022-07-10-Duncolm.gpx",
+					"display_url": "johnjohnston.info/walks/maps.htm…",
+					"indices": [
+						89,
+						112
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1546182525642543000,
+					"id_str": "1546182525642543104",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/media/FXUk0KbXEAAOCzl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXUk0KbXEAAOCzl.jpg",
+					"url": "https://t.co/srh9hebhv1",
+					"display_url": "pic.twitter.com/srh9hebhv1",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1546182529585225735/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 620,
+							"h": 620,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 620,
+							"h": 620,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 620,
+							"h": 620,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1546182525642543000,
+					"id_str": "1546182525642543104",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/media/FXUk0KbXEAAOCzl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXUk0KbXEAAOCzl.jpg",
+					"url": "https://t.co/srh9hebhv1",
+					"display_url": "pic.twitter.com/srh9hebhv1",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1546182529585225735/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 620,
+							"h": 620,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 620,
+							"h": 620,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 620,
+							"h": 620,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1546182528008061000,
+					"id_str": "1546182528008060934",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/media/FXUk0TPWAAYbJqQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXUk0TPWAAYbJqQ.jpg",
+					"url": "https://t.co/srh9hebhv1",
+					"display_url": "pic.twitter.com/srh9hebhv1",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1546182529585225735/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://micro.blog/\" rel=\"nofollow\">Micro.blog</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3895,
+			"friends_count": 2701,
+			"listed_count": 258,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 6233,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19522,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Back to Old Kilpatrick this morning to walk to Duncolm. Notes, photos &amp; map: walkmap https://t.co/3LhP0hQnmZ https://t.co/srh9hebhv1"
+	},
+	{
 		"created_at": "Fri Jul 08 17:25:07 +0000 2022",
 		"id": 1545458976271335400,
 		"id_str": "1545458976271335427",
