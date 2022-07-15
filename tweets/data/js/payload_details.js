b415c3354d16594deba504cbb8aeae12e8d32327
John Johnston <johnjohnston@mac.com>
2022-07-15 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19131,
-	"created_at": "Thu Jul 14 00:17:47 +0000 2022",
+	"tweets": 19132,
+	"created_at": "Fri Jul 15 00:17:47 +0000 2022",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2022_07.js
+++ tweets/data/js/tweets/2022_07.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Thu Jul 14 18:39:18 +0000 2022",
+		"id": 1547651971137474600,
+		"id_str": "1547651971137474568",
+		"full_text": "@TeacherToolkit @IanStuart66 I wonder how #edtech is defined 🤯\n\nFor me: a text editor\nFor my current teaching situation, 1-2-1 iPads: Notes app &amp; airdrop",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			157
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "edtech",
+					"indices": [
+						42,
+						49
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TeacherToolkit",
+					"name": "🇬🇧 TeacherToolkit.co.uk",
+					"id": 180327352,
+					"id_str": "180327352",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "IanStuart66",
+					"name": "Ian Stuart",
+					"id": 2228692152,
+					"id_str": "2228692152",
+					"indices": [
+						16,
+						28
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1547476008626012200,
+		"in_reply_to_status_id_str": "1547476008626012160",
+		"in_reply_to_user_id": 180327352,
+		"in_reply_to_user_id_str": "180327352",
+		"in_reply_to_screen_name": "TeacherToolkit",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3900,
+			"friends_count": 2701,
+			"listed_count": 258,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 6239,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19526,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@TeacherToolkit @IanStuart66 I wonder how #edtech is defined 🤯\n\nFor me: a text editor\nFor my current teaching situation, 1-2-1 iPads: Notes app &amp; airdrop"
+	},
+	{
 		"created_at": "Wed Jul 13 13:18:48 +0000 2022",
 		"id": 1547208926764089300,
 		"id_str": "1547208926764089345",
