2fa9d9dd66e0648661a4f6454d7f4a0598660016
John Johnston <johnjohnston@mac.com>
2022-08-22 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19168,
-	"created_at": "Sat Aug 20 00:17:48 +0000 2022",
+	"tweets": 19169,
+	"created_at": "Mon Aug 22 00:17:47 +0000 2022",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2022_08.js
+++ tweets/data/js/tweets/2022_08.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sun Aug 21 18:59:34 +0000 2022",
+		"id": 1561427811755335700,
+		"id_str": "1561427811755335680",
+		"full_text": "Bookmarked Prettymapp by Christoph Rieke (prettymapp) \nMore infos and  stars at https://t.co/W0ir4gOfRf \n\nWell this is fun. The Feature inage on this post is shrunk &amp; compressed. Via Joe Jennet https://t.co/ww0b1eaAOj https://t.co/X0qSvnHVC9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			221
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/W0ir4gOfRf",
+					"expanded_url": "http://github.com/chrieke/prettymapp",
+					"display_url": "github.com/chrieke/pretty…",
+					"indices": [
+						80,
+						103
+					]
+				},
+				{
+					"url": "https://t.co/ww0b1eaAOj",
+					"expanded_url": "https://chrieke-prettymapp-streamlit-prettymappapp-1k0qxh.streamlitapp.com",
+					"display_url": "…prettymappapp-1k0qxh.streamlitapp.com",
+					"indices": [
+						198,
+						221
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1561427807175065600,
+					"id_str": "1561427807175065601",
+					"indices": [
+						222,
+						245
+					],
+					"media_url": "http://pbs.twimg.com/media/FatOUV0WQAEBRLN.png",
+					"media_url_https": "https://pbs.twimg.com/media/FatOUV0WQAEBRLN.png",
+					"url": "https://t.co/X0qSvnHVC9",
+					"display_url": "pic.twitter.com/X0qSvnHVC9",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1561427811755335680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 620,
+							"h": 620,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 620,
+							"h": 620,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 620,
+							"h": 620,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1561427807175065600,
+					"id_str": "1561427807175065601",
+					"indices": [
+						222,
+						245
+					],
+					"media_url": "http://pbs.twimg.com/media/FatOUV0WQAEBRLN.png",
+					"media_url_https": "https://pbs.twimg.com/media/FatOUV0WQAEBRLN.png",
+					"url": "https://t.co/X0qSvnHVC9",
+					"display_url": "pic.twitter.com/X0qSvnHVC9",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1561427811755335680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 620,
+							"h": 620,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 620,
+							"h": 620,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 620,
+							"h": 620,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://micro.blog/\" rel=\"nofollow\">Micro.blog</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3895,
+			"friends_count": 2701,
+			"listed_count": 258,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 6304,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19573,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Bookmarked Prettymapp by Christoph Rieke (prettymapp) \nMore infos and  stars at https://t.co/W0ir4gOfRf \n\nWell this is fun. The Feature inage on this post is shrunk &amp; compressed. Via Joe Jennet https://t.co/ww0b1eaAOj https://t.co/X0qSvnHVC9"
+	},
+	{
 		"created_at": "Fri Aug 19 22:33:40 +0000 2022",
 		"id": 1560756912735617000,
 		"id_str": "1560756912735617024",
