eca8050104b553c3b0d518b22e325c692654853d
John Johnston <johnjohnston@mac.com>
2022-08-30 01:17:48 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19172,
-	"created_at": "Mon Aug 29 00:17:48 +0000 2022",
+	"tweets": 19173,
+	"created_at": "Tue Aug 30 00:17:47 +0000 2022",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2022_08.js
+++ tweets/data/js/tweets/2022_08.js
@@ -1,6 +1,304 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Mon Aug 29 20:03:18 +0000 2022",
+		"id": 1564342953966968800,
+		"id_str": "1564342953966968837",
+		"full_text": "Brilliant thread, fascinating. When I as young we use to get 1 plain &amp; one pan to suit the taste of different members of our family. https://t.co/bhngXUGXyz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bhngXUGXyz",
+					"expanded_url": "https://twitter.com/cocteautriplets/status/1553062819909799936",
+					"display_url": "twitter.com/cocteautriplet…",
+					"indices": [
+						137,
+						160
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Primary Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, iOS, walks, fauna, flora, open, #ds106 #GlowBlogs Radio #Edutalk   https://t.co/QAJpOIj9ta",
+			"url": "https://t.co/p7vNWSMAoH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p7vNWSMAoH",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/QAJpOIj9ta",
+							"expanded_url": "http://johnjohnston.info",
+							"display_url": "johnjohnston.info",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3900,
+			"friends_count": 2702,
+			"listed_count": 258,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 6307,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19577,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1553062819909800000,
+		"quoted_status_id_str": "1553062819909799936",
+		"quoted_status_permalink": {
+			"url": "https://t.co/bhngXUGXyz",
+			"expanded": "https://twitter.com/cocteautriplets/status/1553062819909799936",
+			"display": "twitter.com/cocteautriplet…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jul 29 17:00:05 +0000 2022",
+			"id": 1553062819909800000,
+			"id_str": "1553062819909799936",
+			"full_text": "The curious history and politics of plain breid. A thread. 🧵👇 https://t.co/yz9kteVZra",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				61
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1553060489260945400,
+						"id_str": "1553060489260945410",
+						"indices": [
+							62,
+							85
+						],
+						"media_url": "http://pbs.twimg.com/media/FY2USVkX0AIDlde.png",
+						"media_url_https": "https://pbs.twimg.com/media/FY2USVkX0AIDlde.png",
+						"url": "https://t.co/yz9kteVZra",
+						"display_url": "pic.twitter.com/yz9kteVZra",
+						"expanded_url": "https://twitter.com/cocteautriplets/status/1553062819909799936/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 500,
+								"h": 500,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 500,
+								"h": 500,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 500,
+								"h": 500,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1553060489260945400,
+						"id_str": "1553060489260945410",
+						"indices": [
+							62,
+							85
+						],
+						"media_url": "http://pbs.twimg.com/media/FY2USVkX0AIDlde.png",
+						"media_url_https": "https://pbs.twimg.com/media/FY2USVkX0AIDlde.png",
+						"url": "https://t.co/yz9kteVZra",
+						"display_url": "pic.twitter.com/yz9kteVZra",
+						"expanded_url": "https://twitter.com/cocteautriplets/status/1553062819909799936/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 500,
+								"h": 500,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 500,
+								"h": 500,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 500,
+								"h": 500,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 98947395,
+				"id_str": "98947395",
+				"name": "Andy Arthur 💃🏰🦌",
+				"screen_name": "cocteautriplets",
+				"location": "The Barony of Restalrig",
+				"description": "I write threads on the more obscure or wacky bits of Edinburgh & Leith local history and Scottish modern history. Expect food, trains, old buildings and maps.",
+				"url": "https://t.co/WuBV9lqHjV",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/WuBV9lqHjV",
+								"expanded_url": "https://ko-fi.com/andyarthur",
+								"display_url": "ko-fi.com/andyarthur",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 10912,
+				"friends_count": 2250,
+				"listed_count": 125,
+				"created_at": "Wed Dec 23 19:47:40 +0000 2009",
+				"favourites_count": 9577,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 118221,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "DFEBEA",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1449358675722838017/qFKwiwjc_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1449358675722838017/qFKwiwjc_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/98947395/1608487055",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 831,
+			"favorite_count": 3587,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Brilliant thread, fascinating. When I as young we use to get 1 plain &amp; one pan to suit the taste of different members of our family. https://t.co/bhngXUGXyz"
+	},
+	{
 		"created_at": "Sun Aug 28 12:57:47 +0000 2022",
 		"id": 1563873477291114500,
 		"id_str": "1563873477291114500",
