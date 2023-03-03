14c5f9e7481fe7aee74efbd924d49dee0b25fbe8
John Johnston <johnjohnston@mac.com>
2023-03-03 00:17:49 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19326,
-	"created_at": "Wed Mar 01 00:17:47 +0000 2023",
+	"tweets": 19327,
+	"created_at": "Fri Mar 03 00:17:48 +0000 2023",
 	"lang": "en"
 }
---
+++ tweets/data/js/tweets/2023_03.js
@@ -0,0 +1,361 @@
+Grailbird.data.tweets_2023_03 = 
+[
+	{
+		"created_at": "Thu Mar 02 19:48:09 +0000 2023",
+		"id": 1631380895218147300,
+		"id_str": "1631380895218147349",
+		"full_text": "These are available now in #GlowBlogs I've added a couple of simple examples:\nhttps://t.co/e6CQqXt9UD\n@GlowScot #GlowScot https://t.co/gj7Jex7Ugk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			121
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GlowBlogs",
+					"indices": [
+						27,
+						37
+					]
+				},
+				{
+					"text": "GlowScot",
+					"indices": [
+						112,
+						121
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GlowScot",
+					"name": "Glow Scot",
+					"id": 2912429973,
+					"id_str": "2912429973",
+					"indices": [
+						102,
+						111
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/e6CQqXt9UD",
+					"expanded_url": "https://blogs.glowscotland.org.uk/glowblogs/h5pexamples/examples/",
+					"display_url": "blogs.glowscotland.org.uk/glowblogs/h5pe…",
+					"indices": [
+						78,
+						101
+					]
+				},
+				{
+					"url": "https://t.co/gj7Jex7Ugk",
+					"expanded_url": "https://twitter.com/H5PTechnology/status/1630542629107650561",
+					"display_url": "twitter.com/H5PTechnology/…",
+					"indices": [
+						122,
+						145
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, walks, fauna, flora, #ds106 #GlowBlogs Radio #Edutalk https://t.co/tRyfXqXdny",
+			"url": "https://t.co/OdmqeSa3gG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OdmqeSa3gG",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/tRyfXqXdny",
+							"expanded_url": "https://social.ds106.us/@johnjohnston",
+							"display_url": "social.ds106.us/@johnjohnston",
+							"indices": [
+								117,
+								140
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3835,
+			"friends_count": 2675,
+			"listed_count": 257,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 6358,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19779,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1630542629107650600,
+		"quoted_status_id_str": "1630542629107650561",
+		"quoted_status_permalink": {
+			"url": "https://t.co/gj7Jex7Ugk",
+			"expanded": "https://twitter.com/H5PTechnology/status/1630542629107650561",
+			"display": "twitter.com/H5PTechnology/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Feb 28 12:17:11 +0000 2023",
+			"id": 1630542629107650600,
+			"id_str": "1630542629107650561",
+			"full_text": "🎉🚀 Exciting news! H5P just launched new features in the February release! 🔍Image Choice in Quiz, 🔀Flashcards in random order, 📝Improved Document export! Read full release notes here: https://t.co/LVJ90DdcMy Try it out today! #H5P #edtech https://t.co/9Tm97EYvy9",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				237
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "H5P",
+						"indices": [
+							225,
+							229
+						]
+					},
+					{
+						"text": "edtech",
+						"indices": [
+							230,
+							237
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/LVJ90DdcMy",
+						"expanded_url": "https://h5p.org/february-2023-release-note",
+						"display_url": "h5p.org/february-2023-…",
+						"indices": [
+							183,
+							206
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1630542541387968500,
+						"id_str": "1630542541387968514",
+						"indices": [
+							238,
+							261
+						],
+						"media_url": "http://pbs.twimg.com/media/FqDZzWqXsAI4Rr8.png",
+						"media_url_https": "https://pbs.twimg.com/media/FqDZzWqXsAI4Rr8.png",
+						"url": "https://t.co/9Tm97EYvy9",
+						"display_url": "pic.twitter.com/9Tm97EYvy9",
+						"expanded_url": "https://twitter.com/H5PTechnology/status/1630542629107650561/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 720,
+								"h": 360,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 360,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1630542541387968500,
+						"id_str": "1630542541387968514",
+						"indices": [
+							238,
+							261
+						],
+						"media_url": "http://pbs.twimg.com/media/FqDZzWqXsAI4Rr8.png",
+						"media_url_https": "https://pbs.twimg.com/media/FqDZzWqXsAI4Rr8.png",
+						"url": "https://t.co/9Tm97EYvy9",
+						"display_url": "pic.twitter.com/9Tm97EYvy9",
+						"expanded_url": "https://twitter.com/H5PTechnology/status/1630542629107650561/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 720,
+								"h": 360,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 360,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2216405440,
+				"id_str": "2216405440",
+				"name": "H5P",
+				"screen_name": "H5PTechnology",
+				"location": "Norway",
+				"description": "Free open source technology for creating, sharing and reusing Interactive Content.  #WordPressPlugin #DrupalModule #MoodlePlugin #HTML5  #eLearning #edtech",
+				"url": "http://t.co/3UezpBYiOv",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/3UezpBYiOv",
+								"expanded_url": "http://h5p.org",
+								"display_url": "h5p.org",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5526,
+				"friends_count": 723,
+				"listed_count": 149,
+				"created_at": "Mon Dec 09 20:54:51 +0000 2013",
+				"favourites_count": 6024,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1646,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/453821604435210240/MsPMRMXu_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/453821604435210240/MsPMRMXu_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2216405440/1443687429",
+				"profile_link_color": "E4008D",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 17,
+			"favorite_count": 27,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "These are available now in #GlowBlogs I've added a couple of simple examples:\nhttps://t.co/e6CQqXt9UD\n@GlowScot #GlowScot https://t.co/gj7Jex7Ugk"
+	}
+]
