e4a112e3a001ba6f671d61e1085a868617872c6d
John Johnston <johnjohnston@mac.com>
2023-03-04 00:17:47 +0000

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19327,
-	"created_at": "Fri Mar 03 00:17:48 +0000 2023",
+	"tweets": 19328,
+	"created_at": "Sat Mar 04 00:17:47 +0000 2023",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2023_03.js
+++ tweets/data/js/tweets/2023_03.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Fri Mar 03 18:31:14 +0000 2023",
+		"id": 1631723926249250800,
+		"id_str": "1631723926249250842",
+		"full_text": "@ds106dc Not a robot #tdc4067 #ds106 https://t.co/dDmsrWItCv",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			36
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tdc4067",
+					"indices": [
+						21,
+						29
+					]
+				},
+				{
+					"text": "ds106",
+					"indices": [
+						30,
+						36
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ds106dc",
+					"name": "ds106 Daily Create",
+					"id": 995562187,
+					"id_str": "995562187",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1631723748821803000,
+					"id_str": "1631723748821803023",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FqUMGrQXoA8i4q0.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FqUMGrQXoA8i4q0.jpg",
+					"url": "https://t.co/dDmsrWItCv",
+					"display_url": "pic.twitter.com/dDmsrWItCv",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1631723926249250842/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 284,
+							"h": 410,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 284,
+							"h": 410,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 284,
+							"h": 410,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1631723748821803000,
+					"id_str": "1631723748821803023",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FqUMGrQXoA8i4q0.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FqUMGrQXoA8i4q0.jpg",
+					"url": "https://t.co/dDmsrWItCv",
+					"display_url": "pic.twitter.com/dDmsrWItCv",
+					"expanded_url": "https://twitter.com/johnjohnston/status/1631723926249250842/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"large": {
+							"w": 284,
+							"h": 410,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 284,
+							"h": 410,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 284,
+							"h": 410,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							142,
+							205
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FqUMGrQXoA8i4q0.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1631602735429697500,
+		"in_reply_to_status_id_str": "1631602735429697536",
+		"in_reply_to_user_id": 995562187,
+		"in_reply_to_user_id_str": "995562187",
+		"in_reply_to_screen_name": "ds106dc",
+		"user": {
+			"id": 7484192,
+			"id_str": "7484192",
+			"name": "john johnston",
+			"screen_name": "johnjohnston",
+			"location": "Glasgow, UK",
+			"description": "Teacher, @banton_pr, ADE, blogging, podcasting, rss, web, mac, walks, fauna, flora, #ds106 #GlowBlogs Radio #Edutalk https://t.co/tRyfXqXdny",
+			"url": "https://t.co/OdmqeSa3gG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OdmqeSa3gG",
+							"expanded_url": "http://johnjohnston.info/blog/",
+							"display_url": "johnjohnston.info/blog/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/tRyfXqXdny",
+							"expanded_url": "https://social.ds106.us/@johnjohnston",
+							"display_url": "social.ds106.us/@johnjohnston",
+							"indices": [
+								117,
+								140
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3834,
+			"friends_count": 2675,
+			"listed_count": 257,
+			"created_at": "Sun Jul 15 09:03:32 +0000 2007",
+			"favourites_count": 6361,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19780,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EE6C00",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/764409646526427136/wENlgXJ3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7484192/1380997488",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "FFFF00",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@ds106dc Not a robot #tdc4067 #ds106 https://t.co/dDmsrWItCv"
+	},
+	{
 		"created_at": "Thu Mar 02 19:48:09 +0000 2023",
 		"id": 1631380895218147300,
 		"id_str": "1631380895218147349",
