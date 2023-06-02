567cb39bdf7db4caf918472b28185534294b59b0
John Johnston <johnjohnston@mac.com>
2023-06-02 01:17:49 +0100

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19371,
-	"created_at": "Thu Jun 01 00:17:48 +0000 2023",
+	"tweets": 19372,
+	"created_at": "Fri Jun 02 00:17:48 +0000 2023",
 	"lang": "en"
 }
---
+++ tweets/data/js/tweets/2023_06.js
@@ -0,0 +1,206 @@
+Grailbird.data.tweets_2023_06 = 
+[
+	{
+		"created_at": "Thu Jun 01 20:15:17 +0000 2023",
+		"id": 1664361839839400000,
+		"id_str": "1664365019289485313",
+		"full_text": "I’ve been very lucky to film &amp; direct a new doc on the amazing Natalie &amp; Stevie Ray. 7.30PM, BBC One tomorrow! A joy to work with  @StoryboardTVUK @SpeckyProducti1 thank you so much for the support &amp; thank you @BBCScotland @screenscots for creating this amazing opportunity ❤️ https://t.co/0hlQDlgT3A",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			288
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StoryboardTVUK",
+					"name": "Storyboard Studios",
+					"id": 1354457828245840000,
+					"id_str": "1354457828245839873",
+					"indices": [
+						139,
+						154
+					]
+				},
+				{
+					"screen_name": "SpeckyProducti1",
+					"name": "Specky Productions",
+					"id": 1381931942531174400,
+					"id_str": "1381931942531174409",
+					"indices": [
+						155,
+						171
+					]
+				},
+				{
+					"screen_name": "BBCScotland",
+					"name": "BBC Scotland",
+					"id": 622133,
+					"id_str": "622133",
+					"indices": [
+						222,
+						234
+					]
+				},
+				{
+					"screen_name": "screenscots",
+					"name": "Screen Scotland",
+					"id": 971681379103727600,
+					"id_str": "971681379103727616",
+					"indices": [
+						235,
+						247
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1664361834906828800,
+					"id_str": "1664361834906828803",
+					"indices": [
+						289,
+						312
+					],
+					"media_url": "http://pbs.twimg.com/media/FxkARUtWwAM9N3v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FxkARUtWwAM9N3v.jpg",
+					"url": "https://t.co/0hlQDlgT3A",
+					"display_url": "pic.twitter.com/0hlQDlgT3A",
+					"expanded_url": "https://twitter.com/cmdjohnston/status/1664361839839399937/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 455,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 722,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 722,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1664361834906828800,
+					"id_str": "1664361834906828803",
+					"indices": [
+						289,
+						312
+					],
+					"media_url": "http://pbs.twimg.com/media/FxkARUtWwAM9N3v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FxkARUtWwAM9N3v.jpg",
+					"url": "https://t.co/0hlQDlgT3A",
+					"display_url": "pic.twitter.com/0hlQDlgT3A",
+					"expanded_url": "https://twitter.com/cmdjohnston/status/1664361839839399937/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 455,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 722,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 722,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 500161865,
+			"id_str": "500161865",
+			"name": "Christine Johnston",
+			"screen_name": "cmdjohnston",
+			"location": "Glasgow",
+			"description": "Producer/Director. Glasgow. All views my own etc etc",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 171,
+			"friends_count": 609,
+			"listed_count": 0,
+			"created_at": "Wed Feb 22 21:27:33 +0000 2012",
+			"favourites_count": 2581,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 632,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "ACDED6",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1389292153696890884/-Sn1guCY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1389292153696890884/-Sn1guCY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/500161865/1403041031",
+			"profile_link_color": "038543",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "F6F6F6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I’ve been very lucky to film &amp; direct a new doc on the amazing Natalie &amp; Stevie Ray. 7.30PM, BBC One tomorrow! A joy to work with  @StoryboardTVUK @SpeckyProducti1 thank you so much for the support &amp; thank you @BBCScotland @screenscots for creating this amazing opportunity ❤️ https://t.co/0hlQDlgT3A"
+	}
+]
